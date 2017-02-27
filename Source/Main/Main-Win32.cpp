0b0323906c4a0a6e9978bde19f7bc076dbc2ab78
Andre Duvoisin <anduv13@gmail.com>
2017-02-26 19:17:05 -0600

    DirectX initial commit.

--- Source/Main/Main-Win32.cpp
+++ Source/Main/Main-Win32.cpp
@@ -6,18 +6,264 @@
 
 #include <Windows.h>
 
-int WINAPI wWinMain(
-	HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPWSTR cmdLine,
-	int cmdShow)
+
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+#include <DirectXColors.h>
+
+// TODO: Write our own shit.
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+
+// Link library dependencies
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "winmm.lib")
+
+
+template<typename T>
+inline void SafeRelease(T& ptr)
 {
-	CE_UNUSED(hInstance);
-	CE_UNUSED(hPrevInstance);
-	CE_UNUSED(cmdLine);
-	CE_UNUSED(cmdShow);
+	if (ptr != NULL)
+	{
+		ptr->Release();
+		ptr = NULL;
+	}
+}
+
+
+const LONG g_WindowWidth = 1280;
+const LONG g_WindowHeight = 720;
+LPCSTR g_WindowClassName = "DirectXWindowClass";
+LPCSTR g_WindowName = "DirectX Template";
+HWND g_WindowHandle = 0;
+
+const BOOL g_EnableVSync = FALSE;
+
+
+// Direct3D device and swap chain.
+ID3D11Device* g_d3dDevice = nullptr;
+ID3D11DeviceContext* g_d3dDeviceContext = nullptr;
+IDXGISwapChain* g_d3dSwapChain = nullptr;
+
+// Render target view for the back buffer of the swap chain.
+ID3D11RenderTargetView* g_d3dRenderTargetView = nullptr;
+// Depth/stencil view for use as a depth buffer.
+ID3D11DepthStencilView* g_d3dDepthStencilView = nullptr;
+// A texture to associate to the depth stencil view.
+ID3D11Texture2D* g_d3dDepthStencilBuffer = nullptr;
+
+// Define the functionality of the depth/stencil stages.
+ID3D11DepthStencilState* g_d3dDepthStencilState = nullptr;
+// Define the functionality of the rasterizer stage.
+ID3D11RasterizerState* g_d3dRasterizerState = nullptr;
+D3D11_VIEWPORT g_Viewport = {0};
+
+
+// Vertex buffer data
+ID3D11InputLayout* g_d3dInputLayout = nullptr;
+ID3D11Buffer* g_d3dVertexBuffer = nullptr;
+ID3D11Buffer* g_d3dIndexBuffer = nullptr;
+
+// Shader data
+ID3D11VertexShader* g_d3dVertexShader = nullptr;
+ID3D11PixelShader* g_d3dPixelShader = nullptr;
+
+
+// Shader resources
+enum ConstantBuffer
+{
+	CB_Application,
+	CB_Frame,
+	CB_Object,
+	NumConstantBuffers
+};
+
+ID3D11Buffer* g_d3dConstantBuffers[NumConstantBuffers];
+
+
+using namespace DirectX;
+
+
+// Demo parameters
+XMMATRIX g_WorldMatrix;
+XMMATRIX g_ViewMatrix;
+XMMATRIX g_ProjectionMatrix;
+
+
+// Vertex data for a colored cube.
+struct VertexPosColor
+{
+	XMFLOAT3 Position;
+	XMFLOAT3 Color;
+};
+
+VertexPosColor g_Vertices[8] =
+{
+	{XMFLOAT3(-1.0f, -1.0f, -1.0f), XMFLOAT3(0.0f, 0.0f, 0.0f)}, // 0
+	{XMFLOAT3(-1.0f,  1.0f, -1.0f), XMFLOAT3(0.0f, 1.0f, 0.0f)}, // 1
+	{XMFLOAT3(1.0f,  1.0f, -1.0f), XMFLOAT3(1.0f, 1.0f, 0.0f)}, // 2
+	{XMFLOAT3(1.0f, -1.0f, -1.0f), XMFLOAT3(1.0f, 0.0f, 0.0f)}, // 3
+	{XMFLOAT3(-1.0f, -1.0f,  1.0f), XMFLOAT3(0.0f, 0.0f, 1.0f)}, // 4
+	{XMFLOAT3(-1.0f,  1.0f,  1.0f), XMFLOAT3(0.0f, 1.0f, 1.0f)}, // 5
+	{XMFLOAT3(1.0f,  1.0f,  1.0f), XMFLOAT3(1.0f, 1.0f, 1.0f)}, // 6
+	{XMFLOAT3(1.0f, -1.0f,  1.0f), XMFLOAT3(1.0f, 0.0f, 1.0f)}  // 7
+};
+
+WORD g_Indicies[36] =
+{
+	0, 1, 2, 0, 2, 3,
+	4, 6, 5, 4, 7, 6,
+	4, 5, 1, 4, 1, 0,
+	3, 2, 6, 3, 6, 7,
+	1, 5, 6, 1, 6, 2,
+	4, 0, 3, 4, 3, 7
+};
+
+
+// Forward declarations.
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+template<class ShaderClass>
+ShaderClass* LoadShader(const std::wstring& fileName, const std::string& entryPoint, const std::string& profile);
+
+bool LoadContent();
+void UnloadContent();
+
+void Update(float deltaTime);
+void Render();
+void Cleanup();
+
+
+/**
+* Initialize the application window.
+*/
+int InitApplication(HINSTANCE hInstance, int cmdShow)
+{
+	WNDCLASSEX wndClass = {0};
+	wndClass.cbSize = sizeof(WNDCLASSEX);
+	wndClass.style = CS_HREDRAW | CS_VREDRAW;
+	wndClass.lpfnWndProc = &WndProc;
+	wndClass.hInstance = hInstance;
+	wndClass.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wndClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wndClass.lpszMenuName = nullptr;
+	wndClass.lpszClassName = g_WindowClassName;
+
+	if (!RegisterClassEx(&wndClass))
+	{
+		return -1;
+	}
+	
+	RECT windowRect = {0, 0, g_WindowWidth, g_WindowHeight};
+	AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, FALSE);
+
+	g_WindowHandle = CreateWindowA(g_WindowClassName, g_WindowName,
+		WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+		windowRect.right - windowRect.left,
+		windowRect.bottom - windowRect.top,
+		nullptr, nullptr, hInstance, nullptr);
+
+	if (!g_WindowHandle)
+	{
+		return -1;
+	}
+
+	ShowWindow(g_WindowHandle, cmdShow);
+	UpdateWindow(g_WindowHandle);
 
 	return 0;
+}
+
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT paintStruct;
+	HDC hDC;
+
+	switch (message)
+	{
+		case WM_PAINT:
+			{
+				hDC = BeginPaint(hwnd, &paintStruct);
+				EndPaint(hwnd, &paintStruct);
+			}
+			break;
+		case WM_DESTROY:
+			{
+				PostQuitMessage(0);
+			}
+			break;
+		default:
+			return DefWindowProc(hwnd, message, wParam, lParam);
+	}
+
+	return 0;
+}
+
+
+/**
+* The main application loop.
+*/
+int Run()
+{
+	MSG msg = {0};
+
+	static DWORD previousTime = timeGetTime();
+	static const float targetFramerate = 30.0f;
+	static const float maxTimeStep = 1.0f / targetFramerate;
+
+	while (msg.message != WM_QUIT)
+	{
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else
+		{
+			DWORD currentTime = timeGetTime();
+			float deltaTime = (currentTime - previousTime) / 1000.0f;
+			previousTime = currentTime;
+
+			// Cap the delta time to the max time step (useful if you're 
+			// debugging and you don't want the deltaTime value to explode).
+			deltaTime = std::min<float>(deltaTime, maxTimeStep);
+
+			//Update(deltaTime);
+			//Render();
+		}
+	}
+
+	return static_cast<int>(msg.wParam);
+}
+
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE prevInstance, LPWSTR cmdLine, int cmdShow)
+{
+	UNREFERENCED_PARAMETER(prevInstance);
+	UNREFERENCED_PARAMETER(cmdLine);
+
+	// Check for DirectX Math library support.
+	if (!XMVerifyCPUSupport())
+	{
+		MessageBox(nullptr, TEXT("Failed to verify DirectX Math library support."), TEXT("Error"), MB_OK);
+		return -1;
+	}
+
+	if (InitApplication(hInstance, cmdShow) != 0)
+	{
+		MessageBox(nullptr, TEXT("Failed to create application window."), TEXT("Error"), MB_OK);
+		return -1;
+	}
+
+	int returnCode = Run();
+
+	return returnCode;
 }
 
 #endif
