6f530e11b074b4a3d86758d43d9911c346f39f71
Andre Duvoisin <anduv13@gmail.com>
2017-09-09 14:12:14 -0500

    Reorganize print functions.

--- src/main/Main.cpp
+++ src/main/Main.cpp
@@ -38,65 +38,63 @@ GLuint g_ibo = 0;
 void printProgramLog(GLuint program)
 {
 	//Make sure name is shader
-	if (glIsProgram(program))
-	{
-		//Program log length
-		int infoLogLength = 0;
-		int maxLength = infoLogLength;
-
-		//Get info string length
-		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &maxLength);
-
-		//Allocate string
-		char* infoLog = new char[maxLength];
-
-		//Get info log
-		glGetProgramInfoLog(program, maxLength, &infoLogLength, infoLog);
-		if (infoLogLength > 0)
-		{
-			//Print Log
-			printf("%s\n", infoLog);
-		}
-
-		//Deallocate string
-		delete[] infoLog;
-	}
-	else
+	if (!glIsProgram(program))
 	{
 		printf("Name %d is not a program\n", program);
+		return;
 	}
+
+	//Program log length
+	int infoLogLength = 0;
+	int maxLength = infoLogLength;
+
+	//Get info string length
+	glGetProgramiv(program, GL_INFO_LOG_LENGTH, &maxLength);
+
+	//Allocate string
+	char* infoLog = new char[maxLength];
+
+	//Get info log
+	glGetProgramInfoLog(program, maxLength, &infoLogLength, infoLog);
+	if (infoLogLength > 0)
+	{
+		//Print Log
+		printf("%s\n", infoLog);
+	}
+
+	//Deallocate string
+	delete[] infoLog;
 }
 
 void printShaderLog(GLuint shader)
 {
 	//Make sure name is shader
-	if (glIsShader(shader))
-	{
-		//Shader log length
-		int infoLogLength = 0;
-		int maxLength = infoLogLength;
-
-		//Get info string length
-		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &maxLength);
-
-		//Allocate string
-		char* infoLog = new char[maxLength];
-
-		//Get info log
-		glGetShaderInfoLog(shader, maxLength, &infoLogLength, infoLog);
-		if (infoLogLength > 0)
-		{
-			//Print Log
-			printf("%s\n", infoLog);
-		}
-
-		//Deallocate string
-		delete[] infoLog;
-	}
-	else
+	if (!glIsShader(shader))
 	{
 		printf("Name %d is not a shader\n", shader);
+		return;
 	}
+
+	//Shader log length
+	int infoLogLength = 0;
+	int maxLength = infoLogLength;
+
+	//Get info string length
+	glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &maxLength);
+
+	//Allocate string
+	char* infoLog = new char[maxLength];
+
+	//Get info log
+	glGetShaderInfoLog(shader, maxLength, &infoLogLength, infoLog);
+	if (infoLogLength > 0)
+	{
+		//Print Log
+		printf("%s\n", infoLog);
+	}
+
+	//Deallocate string
+	delete[] infoLog;
 }
 
 void HandleKeys(unsigned char key, int x, int y)
