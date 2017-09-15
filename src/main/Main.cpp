d7a27b1a1a37d5ba81d3915cfc92cd039b264f0b
Andre Duvoisin <anduv13@gmail.com>
2017-09-14 23:14:31 -0500

    Batman draws with SDL and OpenGL!

--- src/main/Main.cpp
+++ src/main/Main.cpp
@@ -2,10 +2,15 @@
 #include <GL\glew.h>
 #include <SDL_opengl.h>
 #include <gl\GLU.h>
+#include <glm\glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 #include <string>
 
 #include <cstdio>
+
+#include "graphics\MeshManager.h"
+#include "graphics\MeshData.h"
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
@@ -31,9 +36,12 @@ SDL_Surface* g_currentSurface = NULL;
 bool g_renderQuad = true;
 
 GLuint g_programID = 0;
-GLint g_vertexPos2DLocation = -1;
+//GLint g_vertexPos2DLocation = -1;
 GLuint g_vbo = 0;
 GLuint g_ibo = 0;
+GLuint g_vao = 0;
+
+GLuint g_mvpMatrixID = -1;
 
 void printProgramLog(GLuint program)
 {
@@ -113,46 +121,46 @@ void Update()
 void Render()
 {
 	//Clear color buffer
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	//Render quad
-	if (g_renderQuad)
-	{
-		//Bind program
-		glUseProgram(g_programID);
+	//if (g_renderQuad)
+	//{
+	//	//Bind program
+	//	glUseProgram(g_programID);
 
-		//Enable vertex position
-		glEnableVertexAttribArray(g_vertexPos2DLocation);
+	//	//Enable vertex position
+	//	glEnableVertexAttribArray(g_vertexPos2DLocation);
 
-		//Set vertex data
-		glBindBuffer(GL_ARRAY_BUFFER, g_vbo);
-		glVertexAttribPointer(g_vertexPos2DLocation, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), NULL);
+	//	//Set vertex data
+	//	glBindBuffer(GL_ARRAY_BUFFER, g_vbo);
+	//	glVertexAttribPointer(g_vertexPos2DLocation, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), NULL);
 
-		//Set index data and render
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, g_ibo);
-		glDrawElements(GL_TRIANGLE_FAN, 4, GL_UNSIGNED_INT, NULL);
+	//	//Set index data and render
+	//	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, g_ibo);
+	//	glDrawElements(GL_TRIANGLE_FAN, 4, GL_UNSIGNED_INT, NULL);
 
-		//Disable vertex position
-		glDisableVertexAttribArray(g_vertexPos2DLocation);
+	//	//Disable vertex position
+	//	glDisableVertexAttribArray(g_vertexPos2DLocation);
 
-		//Unbind program
-		glUseProgram(NULL);
-	}
+	//	//Unbind program
+	//	glUseProgram(NULL);
+	//}
 
-	// OpenGL 2.1
-	/*
-	glClear(GL_COLOR_BUFFER_BIT);
+	glUseProgram(g_programID);
 
-	if (g_renderQuad)
-	{
-		glBegin(GL_QUADS);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glVertex2f(0.5f, 0.5f);
-		glVertex2f(-0.5f, 0.5f);
-		glEnd();
-	}
-	*/
+	glm::mat4 projection = glm::perspective(glm::pi<float>() * 0.25f, (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
+	glm::mat4 view = glm::lookAt(glm::vec3(4, 3, 3), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+	glm::mat4 model = glm::mat4(1.0f);
+	glm::mat4 mvp = projection * view * model;
+	glUniformMatrix4fv(g_mvpMatrixID, 1, GL_FALSE, &mvp[0][0]);
+		
+	glBindVertexArray(g_vao);
+
+	CE::MeshData* meshData = CE::MeshManager::Get().GetMeshData("..\\..\\..\\Assets\\watcher.fbx");
+
+	//glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDrawArrays(GL_TRIANGLES, 0, meshData->m_vertices.size());
 }
 
 bool InitializeOpenGL()
@@ -160,11 +168,18 @@ bool InitializeOpenGL()
 	g_programID = glCreateProgram();
 
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	const GLchar* vertexShaderSource[] =
-	{
-		"#version 140\nin vec2 LVertexPos2D; void main() { gl_Position = vec4(LVertexPos2D.x, LVertexPos2D.y, 0, 1); }"
-	};
-	glShaderSource(vertexShader, 1, vertexShaderSource, NULL);
+	//const GLchar* vertexShaderSource[] =
+	//{
+	//	"#version 410\nin vec2 LVertexPos2D; void main() { gl_Position = vec4(LVertexPos2D.x, LVertexPos2D.y, 0, 1); }"
+	//};
+	const char* vertexShaderSource =
+		"#version 410\n"
+		"in vec3 vp;"
+		"uniform mat4 mvp;"
+		"void main() {"
+		"  gl_Position = mvp * vec4(vp, 1.0);"
+		"}";
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
 	GLint vShaderCompiled = GL_FALSE;
 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &vShaderCompiled);
@@ -177,11 +192,17 @@ bool InitializeOpenGL()
 	glAttachShader(g_programID, vertexShader);
 
 	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	const GLchar* fragmentShaderSource[] =
-	{
-		"#version 140\nout vec4 LFragment; void main() { LFragment = vec4(1.0, 1.0, 1.0, 1.0); }"
-	};
-	glShaderSource(fragmentShader, 1, fragmentShaderSource, NULL);
+	//const GLchar* fragmentShaderSource[] =
+	//{
+	//	"#version 410\nout vec4 LFragment; void main() { LFragment = vec4(1.0, 1.0, 1.0, 1.0); }"
+	//};
+	const char* fragmentShaderSource =
+		"#version 410\n"
+		"out vec4 frag_colour;"
+		"void main() {"
+		"  frag_colour = vec4(0.5, 0.0, 0.5, 1.0);"
+		"}";
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
 	glCompileShader(fragmentShader);
 	GLint fShaderCompiled = GL_FALSE;
 	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &fShaderCompiled);
@@ -203,70 +224,52 @@ bool InitializeOpenGL()
 		return false;
 	}
 
-	g_vertexPos2DLocation = glGetAttribLocation(g_programID, "LVertexPos2D");
-	if (g_vertexPos2DLocation == -1)
-	{
-		printf("LVertexPos2D is not a valid glsl program variable!\n");
-		return false;
-	}
+	g_mvpMatrixID = glGetUniformLocation(g_programID, "mvp");
+
+	//g_vertexPos2DLocation = glGetAttribLocation(g_programID, "LVertexPos2D");
+	//if (g_vertexPos2DLocation == -1)
+	//{
+	//	printf("LVertexPos2D is not a valid glsl program variable!\n");
+	//	return false;
+	//}
 
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
-	GLfloat vertexData[] =
-	{
-		-0.5f, -0.5f,
-		0.5f, -0.5f,
-		0.5f,  0.5f,
-		-0.5f,  0.5f
+	//GLfloat vertexData[] =
+	//{
+	//	-0.5f, -0.5f,
+	//	0.5f, -0.5f,
+	//	0.5f,  0.5f,
+	//	-0.5f,  0.5f
+	//};
+
+	//GLuint indexData[] = { 0, 1, 2, 3 };
+
+	float points[] = {
+		0.0f,  0.5f,  0.0f,
+		0.5f, -0.5f,  0.0f,
+		-0.5f, -0.5f,  0.0f
 	};
 
-	GLuint indexData[] = { 0, 1, 2, 3 };
+	// "I'm Batman."
+	CE::MeshData* meshData = CE::MeshManager::Get().GetMeshData("..\\..\\..\\Assets\\watcher.fbx");
+
+	glGenVertexArrays(1, &g_vao);
+	glBindVertexArray(g_vao);
 
 	glGenBuffers(1, &g_vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, g_vbo);
-	glBufferData(GL_ARRAY_BUFFER, 2 * 4 * sizeof(GLfloat), vertexData, GL_STATIC_DRAW);
-	
-	glGenBuffers(1, &g_ibo);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, g_ibo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 4 * sizeof(GLuint), indexData, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, meshData->m_vertices.size() * sizeof(CE::MeshData::Vertex), meshData->m_vertices.data(), GL_STATIC_DRAW);
+	//glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	glEnableVertexAttribArray(0);
+
+	//glGenBuffers(1, &g_ibo);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, g_ibo);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, meshData->m_indices.size() * sizeof(WORD), meshData->m_indices.data(), GL_STATIC_DRAW);
+	//glEnableVertexAttribArray(1);
 
 	return true;
-
-	// OpenGL 2.1
-	/*
-	GLenum error = GL_NO_ERROR;
-
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-
-	error = glGetError();
-	if (error != GL_NO_ERROR)
-	{
-		printf("Error initializing OpenGL! %s\n", gluErrorString(error));
-		return false;
-	}
-
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-
-	error = glGetError();
-	if (error != GL_NO_ERROR)
-	{
-		printf("Error initializing OpenGL! %s\n", gluErrorString(error));
-		return false;
-	}
-
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-
-	error = glGetError();
-	if (error != GL_NO_ERROR)
-	{
-		printf("Error initializing OpenGL! %s\n", gluErrorString(error));
-		return false;
-	}
-
-	return true;
-	*/
 }
 
 bool Initialize()
@@ -277,10 +280,12 @@ bool Initialize()
 		return false;
 	}
 
-	// Use OpenGL 2.1
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 
 	g_window = SDL_CreateWindow(
 		"Composite Engine",
@@ -320,17 +325,30 @@ bool Initialize()
 		printf("Warning: Unable to set VSync! SDL_Error: %s\n", SDL_GetError());
 	}
 
+	CE::MeshManager::Get().Initialize();
+
 	if (!InitializeOpenGL())
 	{
 		printf("Unable to initialize OpenGL!\n");
 		return false;
 	}
 
+	const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
+	const GLubyte* version = glGetString(GL_VERSION); // version as a string
+	printf("Renderer: %s\n", renderer);
+	printf("OpenGL version supported %s\n", version);
+
+	// tell GL to only draw onto a pixel if the shape is closer to the viewer
+	glEnable(GL_DEPTH_TEST); // enable depth-testing
+	glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
+
 	return true;
 }
 
 void Destroy()
 {
+	CE::MeshManager::Get().Destroy();
+
 	SDL_DestroyWindow(g_window);
 	g_window = NULL;
 	g_surface = NULL;
