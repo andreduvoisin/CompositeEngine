77d410d4ce1ca2f03de587191805e8e588450452
Andre Duvoisin <anduv13@gmail.com>
2017-11-09 22:02:17 -0600

    Remove SDL surface stuff.

--- src/main/Main.cpp
+++ src/main/Main.cpp
@@ -20,22 +20,7 @@ const int SCREEN_WIDTH = 1280;
 const int SCREEN_HEIGHT = 720;
 
 SDL_Window* g_window = NULL;
-SDL_Surface* g_surface = NULL;
 SDL_GLContext g_context;
-
-enum KeyPressSurfaces
-{
-	KEY_PRESS_SURFACE_DEFAULT,
-	KEY_PRESS_SURFACE_UP,
-	KEY_PRESS_SURFACE_DOWN,
-	KEY_PRESS_SURFACE_LEFT,
-	KEY_PRESS_SURFACE_RIGHT,
-	KEY_PRESS_SURFACE_TOTAL
-};
-
-SDL_Surface* g_keyPressSurfaces[KEY_PRESS_SURFACE_TOTAL];
-
-SDL_Surface* g_currentSurface = NULL;
 
 bool g_renderQuad = true;
 
@@ -335,8 +320,6 @@ bool Initialize()
 		return false;
 	}
 
-	g_surface = SDL_GetWindowSurface(g_window);
-
 	g_context = SDL_GL_CreateContext(g_window);
 
 	if (g_context == NULL)
@@ -389,79 +372,8 @@ void Destroy()
 
 	SDL_DestroyWindow(g_window);
 	g_window = NULL;
-	g_surface = NULL;
 
 	SDL_Quit();
-}
-
-SDL_Surface* LoadSurface(std::string path)
-{
-	SDL_Surface* surface = SDL_LoadBMP(path.c_str());
-
-	if (surface == NULL)
-	{
-		printf("Unable to load image %s! SDL_Error: %s\n", path.c_str(), SDL_GetError());
-		return NULL;
-	}
-
-	SDL_Surface* optimized = SDL_ConvertSurface(surface, g_surface->format, NULL);
-	
-	if (optimized == NULL)
-	{
-		printf("Unable to optimize image %s! SDL_Error: %s\n", path.c_str(), SDL_GetError());
-	}
-
-	SDL_FreeSurface(surface);
-
-	return optimized;
-}
-
-bool LoadMedia()
-{
-	//Loading success flag
-	bool success = true;
-
-	//Load default surface
-	g_keyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT] = LoadSurface("../../../assets/press.bmp");
-	if (g_keyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT] == NULL)
-	{
-		printf("Failed to load default image!\n");
-		success = false;
-	}
-
-	//Load up surface
-	g_keyPressSurfaces[KEY_PRESS_SURFACE_UP] = LoadSurface("../../../assets/up.bmp");
-	if (g_keyPressSurfaces[KEY_PRESS_SURFACE_UP] == NULL)
-	{
-		printf("Failed to load up image!\n");
-		success = false;
-	}
-
-	//Load down surface
-	g_keyPressSurfaces[KEY_PRESS_SURFACE_DOWN] = LoadSurface("../../../assets/down.bmp");
-	if (g_keyPressSurfaces[KEY_PRESS_SURFACE_DOWN] == NULL)
-	{
-		printf("Failed to load down image!\n");
-		success = false;
-	}
-
-	//Load left surface
-	g_keyPressSurfaces[KEY_PRESS_SURFACE_LEFT] = LoadSurface("../../../assets/left.bmp");
-	if (g_keyPressSurfaces[KEY_PRESS_SURFACE_LEFT] == NULL)
-	{
-		printf("Failed to load left image!\n");
-		success = false;
-	}
-
-	//Load right surface
-	g_keyPressSurfaces[KEY_PRESS_SURFACE_RIGHT] = LoadSurface("../../../assets/right.bmp");
-	if (g_keyPressSurfaces[KEY_PRESS_SURFACE_RIGHT] == NULL)
-	{
-		printf("Failed to load right image!\n");
-		success = false;
-	}
-
-	return success;
 }
 
 int main(int argc, char* argv[])
@@ -473,17 +385,8 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 
-	if (!LoadMedia())
-	{
-		printf("Failed to load media.\n");
-		getchar();
-		return -1;
-	}
-
 	bool quit = false;
 	SDL_Event event;
-
-	//g_currentSurface = g_keyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT];
 
 	SDL_StartTextInput();
 
@@ -505,19 +408,14 @@ int main(int argc, char* argv[])
 					//switch (event.key.keysym.sym)
 					//{
 					//	case SDLK_UP:
-					//		g_currentSurface = g_keyPressSurfaces[KEY_PRESS_SURFACE_UP];
 					//		break;
 					//	case SDLK_DOWN:
-					//		g_currentSurface = g_keyPressSurfaces[KEY_PRESS_SURFACE_DOWN];
 					//		break;
 					//	case SDLK_LEFT:
-					//		g_currentSurface = g_keyPressSurfaces[KEY_PRESS_SURFACE_LEFT];
 					//		break;
 					//	case SDLK_RIGHT:
-					//		g_currentSurface = g_keyPressSurfaces[KEY_PRESS_SURFACE_RIGHT];
 					//		break;
 					//	default:
-					//		//g_currentSurface = g_keyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT];
 					//		break;
 					//}
 					break;
@@ -541,14 +439,6 @@ int main(int argc, char* argv[])
 
 		Render();
 		SDL_GL_SwapWindow(g_window);
-
-		//SDL_Rect rect;
-		//rect.x = 0;
-		//rect.y = 0;
-		//rect.w = SCREEN_WIDTH;
-		//rect.h = SCREEN_HEIGHT;
-		//SDL_BlitScaled(g_currentSurface, NULL, g_surface, &rect);
-		//SDL_UpdateWindowSurface(g_window);
 	}
 
 	SDL_StopTextInput();
