44d43d5eea4e6a91db12ef288b0bbe88f843734f
Andre Duvoisin <duvoisin@usc.edu>
2017-12-26 01:23:49 -0600

    Animation bug fixes. Soldier jump still broken.

--- src/main/Main.cpp
+++ src/main/Main.cpp
@@ -37,8 +37,9 @@ GLuint g_paletteGenTex = -1;
 GLuint g_diffuseTextureID = -1;
 GLuint g_diffuseTextureUnit = -1;
 
-const char* g_fbxName = "..\\..\\..\\assets\\Thriller Part 2.fbx";
 //const char* g_fbxName = "..\\..\\..\\assets\\Stand Up.fbx";
+//const char* g_fbxName = "..\\..\\..\\assets\\Soldier_animated_jump.fbx";
+const char* g_fbxName = "..\\..\\..\\assets\\Thriller Part 2.fbx";
 
 void printProgramLog(GLuint program)
 {
@@ -126,8 +127,8 @@ void Render()
 	glm::mat4 projection = glm::perspective(glm::pi<float>() * 0.25f, (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 1000.0f);
 	//glm::mat4 view = glm::lookAt(glm::vec3(0, 100, 400), glm::vec3(0, 100, 0), glm::vec3(0, 1, 0)); // paladin
 	//glm::mat4 view = glm::lookAt(glm::vec3(0, -400, 0), glm::vec3(0, 0, 0), glm::vec3(0, 0, 1)); // solider
-	//glm::mat4 view = glm::lookAt(glm::vec3(0, 2, 8), glm::vec3(0, 2, 0), glm::vec3(0, 1, 0)); // wonder woman
 	glm::mat4 view = glm::lookAt(glm::vec3(0, 200, 700), glm::vec3(0, 50, 0), glm::vec3(0, 1, 0)); // thriller
+	//glm::mat4 view = glm::lookAt(glm::vec3(0, 2, 8), glm::vec3(0, 2, 0), glm::vec3(0, 1, 0)); // wonder woman
 	glm::mat4 model = glm::mat4(1.0f);
 	glm::mat4 projectionViewModel = projection * view * model;
 	glUniformMatrix4fv(g_projectionViewModelMatrixID, 1, GL_FALSE, &projectionViewModel[0][0]);
