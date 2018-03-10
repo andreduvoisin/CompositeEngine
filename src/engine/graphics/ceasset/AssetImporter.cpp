d1cbb88e01cd71a22fa1046b7dabd651a17e7ca2
Andre Duvoisin <anduv13@gmail.com>
2018-03-10 01:14:29 -0600

    Added serializer and deserializer classes for assets.

--- src/engine/graphics/ceasset/AssetImporter.cpp
+++ src/engine/graphics/ceasset/AssetImporter.cpp
@@ -1,15 +1,14 @@
 #include "AssetImporter.h"
 
-#include "AssetTraits.h"
+#include "AssetDeserializer.h"
 #include "InputFileStream.h"
 
-#include "graphics\skeleton\Skeleton.h"
 #include "graphics\mesh\Mesh.h"
 #include "graphics\animation\Animation.h"
-#include "graphics\texture\Texture.h"
 
 namespace CE
 {
+	// TODO: Convert from reference to pointer?
 	bool AssetImporter::ImportSkeletonMeshesAnimationsTexture(
 		const char* fileName, 
 		Skeleton& outSkeleton, 
@@ -24,111 +23,34 @@ namespace CE
 			return false;
 		}
 
-		char header[8];
-		stream.Read(header, 8);
-		if (!(header[0] == 'C'
-			&& header[1] == 'E'
-			&& header[2] == 'A'
-			&& header[3] == 'S'
-			&& header[4] == 'S'
-			&& header[5] == 'E'
-			&& header[6] == 'T'
-			&& header[7] == '\0'))
+		AssetDeserializer deserializer(stream);
+
+		if (!deserializer.ReadAndVerifyHeader())
 		{
 			return false;
 		}
 
 		while (stream.HasData())
 		{
-			const auto assetType = stream.Read<AssetType>();
-
-			switch (assetType)
+			switch (deserializer.ReadAssetType())
 			{
 				case AssetType::SKELETON:
-				{
-					const auto jointCount = stream.Read<unsigned>();
-					outSkeleton.joints.resize(jointCount);
-					for (auto& joint : outSkeleton.joints)
-					{
-						stream >> joint.inverseBindPose;
-						stream >> joint.name;
-						stream >> joint.parentIndex;
-					}
-
+					deserializer.ReadSkeleton(outSkeleton);
 					break;
-				}
 
 				case AssetType::MESH:
-				{
 					outMeshes.push_back(Mesh());
-					Mesh& mesh = outMeshes.back();
-
-					const auto verticesCount = stream.Read<unsigned>();
-					mesh.m_vertices.resize(verticesCount);
-					stream.Read(mesh.m_vertices.data(), verticesCount);
-
-					const auto indicesCount = stream.Read<unsigned>();
-					mesh.m_indices.resize(indicesCount);
-					stream.Read(mesh.m_indices.data(), indicesCount);
-
-					stream >> mesh.m_diffuseMapName;
-					stream >> mesh.m_specularMapName;
-					stream >> mesh.m_normalMapName;
-
+					deserializer.ReadMesh(outMeshes.back());
 					break;
-				}
 
 				case AssetType::ANIMATION:
-				{
 					outAnimations.push_back(Animation());
-					Animation& animation = outAnimations.back();
-
-					stream >> animation.name;
-
-					const auto translationsCount = stream.Read<unsigned>();
-					animation.translations.resize(translationsCount);
-					for (auto& translations : animation.translations)
-					{
-						const auto keyCount = stream.Read<unsigned>();
-						translations.resize(keyCount);
-						stream.Read(translations.data(), keyCount);
-					}
-
-					const auto rotationsCount = stream.Read<unsigned>();
-					animation.rotations.resize(rotationsCount);
-					for (auto& rotations : animation.rotations)
-					{
-						const auto keyCount = stream.Read<unsigned>();
-						rotations.resize(keyCount);
-						stream.Read(rotations.data(), keyCount);
-					}
-
-					const auto scalesCount = stream.Read<unsigned>();
-					animation.scales.resize(scalesCount);
-					for (auto& scales : animation.scales)
-					{
-						const auto keyCount = stream.Read<unsigned>();
-						scales.resize(keyCount);
-						stream.Read(scales.data(), keyCount);
-					}
-
-					stream >> animation.duration;
-
+					deserializer.ReadAnimation(outAnimations.back());
 					break;
-				}
 
 				case AssetType::TEXTURE:
-				{
-					stream >> outTexture.width;
-					stream >> outTexture.height;
-					stream >> outTexture.channels;
-
-					// todo: delete. also probs shouldn't be here anyways
-					outTexture.data = new unsigned char[outTexture.width * outTexture.height * outTexture.channels];
-					stream.Read(outTexture.data, outTexture.width * outTexture.height * outTexture.channels);
-
+					deserializer.ReadTexture(outTexture);
 					break;
-				}
 			}
 		}
 
