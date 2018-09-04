4658ac2b4381ba722a37e6c12227388f1ada15c5
Bedinski <bedinski13@gmail.com>
2018-09-03 19:53:57 -0500

    gotgud with cmake and server runsTM

--- server/test/ServerMain.cpp
+++ server/test/ServerMain.cpp
@@ -21,16 +21,17 @@ void main()
 {
 	init();
 
-	const auto address_family = AF_INET;
-	const auto type = SOCK_DGRAM;
-	const auto protocol = IPPROTO_UDP;
-	const auto sock = socket(address_family, type, protocol);
+	const int address_family = AF_INET;
+	const int type = SOCK_DGRAM;
+	const int protocol = IPPROTO_UDP;
+	SOCKET sock = socket(address_family, type, protocol);
 
 	if (sock == INVALID_SOCKET)
 	{
-		printf("socket failed: %d", WSAGetLastError());
+		printf("socket failed: %d\n", WSAGetLastError());
 		return;
 	}
+	printf("socket is: %i\n", sock);
 
 
 	SOCKADDR_IN local_address;
@@ -43,16 +44,17 @@ void main()
 		printf("bind failed: %d", WSAGetLastError());
 		return;
 	}
+	printf("bind worked!");
 
 	char buffer[SOCKET_BUFFER_SIZE];
 	int flags = 0;
 	SOCKADDR_IN from;
 	int from_size = sizeof(from);
-	auto bytes_received = recvfrom(sock, buffer, SOCKET_BUFFER_SIZE, flags, (SOCKADDR*)&from, &from_size);
+	int bytes_received = recvfrom(sock, buffer, SOCKET_BUFFER_SIZE, flags, (SOCKADDR*)&from, &from_size);
 
 	if (bytes_received == SOCKET_ERROR)
 	{
-		printf("recvfrom returned SOCKET_ERROR, WSAGetLastError() %d", WSAGetLastError());
+		printf("recvfrom returned SOCKET_ERROR, WSAGetLastError() %d\n", WSAGetLastError());
 	}
 	else
 	{
