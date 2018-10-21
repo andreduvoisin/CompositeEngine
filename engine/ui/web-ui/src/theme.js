7a869f36556ece7c041e967873f565a59e6d7167
kevin viglucci <kviglucci@gmail.com>
2018-10-21 12:26:47 -0500

    introduce concept of themes via styled components

--- engine/ui/web-ui/src/theme.js
+++ engine/ui/web-ui/src/theme.js
@@ -1,6 +1,22 @@
+
+const colors = {
+  white: '#fff',
+  black: '#000',
+  transparentBlack: 'rgba(0, 0, 0, 0.5)'
+};
+
+const fonts = {
+  family: '"Encode Sans", sans-serif'
+};
+
+const shared = {
+  background: 'linear-gradient(#1c2534, #161a26)'
+};
+
 const theme = {
-  fg: "palevioletred",
-  bg: "white"
+  colors,
+  fonts,
+  shared
 };
 
 export default theme;
