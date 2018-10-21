ec7336d569c359f86b1c4a0e7f56fb1e07033dc6
kevin viglucci <kviglucci@gmail.com>
2018-10-21 11:56:25 -0500

    add editorconfig and storybook dependencies

--- engine/ui/web-ui/src/App.js
+++ engine/ui/web-ui/src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import Button from './Components/Button';
-import Alert from './Components/Alert';
 import 'font-awesome/css/font-awesome.min.css';
-import { resolve } from 'url';
+import { ThemeProvider } from 'styled-components';
+import theme from './theme';
 
 const sendMessage = (payload) => {
   return new Promise((resolve, reject) => {
@@ -19,23 +18,11 @@ const sendMessage = (payload) => {
 class App extends Component {
   render() {
     return (
-      <div className='WebUi'>
-        <Alert
-          title='Attention'
-          body='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin efficitur ex non dui malesuada, vel mattis nunc accumsan.'
-        >
-          <Button onClick={() => {
-            console.log("button press");
-            sendMessage("my_request")
-              .then((response) => {
-                console.log(response);
-              })
-              .catch((e) => {
-                console.error(e);
-              });
-          }}>Confirm</Button>
-        </Alert>
-      </div>
+      <ThemeProvider theme={theme}>
+        <div className='WebUi'>
+
+        </div>
+      </ThemeProvider>
     );
   }
 }
