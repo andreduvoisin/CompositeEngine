82f203679a246c9451179c87e269920760d7a63f
kevin viglucci <kviglucci@gmail.com>
2018-12-08 01:38:09 -0600

    call  for RENDER_BIND_POSE

--- engine/event/ToggleBindPoseEvent.h
+++ engine/event/ToggleBindPoseEvent.h
@@ -7,10 +7,6 @@ struct ToggleBindPoseEvent : Event
 {
 	ToggleBindPoseEvent();
 	ToggleBindPoseEvent* Clone() const override;
-
-protected:
-	void SerializeInternal(JsonSerializer& serializer) const override;
-	void DeserializeInternal(const JsonDeserializer& deserializer) override;
 };
 
 #endif // _CE_TOGGLE_BIND_POSE_EVENT_H_
