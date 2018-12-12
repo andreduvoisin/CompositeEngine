23f2926fc517074b04a22b2f81aa4279aed50e0f
Andre Duvoisin <anduv13@gmail.com>
2018-12-11 22:10:40 -0600

    Finished up cef subprocess executable.

--- ui/cef/subprocess/UIAppRenderer.h
+++ ui/cef/subprocess/UIAppRenderer.h
@@ -1,14 +1,14 @@
-#ifndef _CE_UI_APP_RENDER_H_
-#define _CE_UI_APP_RENDER_H_
+#ifndef _CE_UI_APP_RENDERER_H_
+#define _CE_UI_APP_RENDERER_H_
 
 #include "UIRenderProcessHandler.h"
 
 #include "include/cef_app.h"
 
-class UIAppRender : public CefApp
+class UIAppRenderer : public CefApp
 {
 public:
-	UIAppRender(CefRefPtr<UIRenderProcessHandler> renderProcessHandler);
+	UIAppRenderer(CefRefPtr<UIRenderProcessHandler> renderProcessHandler);
 
 	// CefApp Interface
 	CefRefPtr<CefRenderProcessHandler> GetRenderProcessHandler() override;
@@ -17,7 +17,8 @@ private:
 	CefRefPtr<UIRenderProcessHandler> renderProcessHandler;
 
 	// IMPLEMENT_* macros set access modifiers, so they must come last.
-	IMPLEMENT_REFCOUNTING(UIAppRender);
+	IMPLEMENT_REFCOUNTING(UIAppRenderer);
+	DISALLOW_COPY_AND_ASSIGN(UIAppRenderer);
 };
 
-#endif //_CE_UI_APP_RENDER_H_
+#endif //_CE_UI_APP_RENDERER_H_
