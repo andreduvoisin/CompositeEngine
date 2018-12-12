42779c28a4a57af030b4c0e22f9db95566ea2448
Andre Duvoisin <anduv13@gmail.com>
2018-12-12 00:52:32 -0600

    Set USE_SANDBOX to OFF for CEF. Add comments to describe why we link to CEF the way we do.

--- cmake/CEF.cmake
+++ cmake/CEF.cmake
@@ -16,8 +16,17 @@ elseif(${CE_PLATFORM} STREQUAL "x64")
 	set(CEF_PLATFORM "x64")
 endif()
 
+# Reference: https://bitbucket.org/chromiumembedded/cef/wiki/LinkingDifferentRunTimeLibraries.md
+#
+# opengl32.lib (and glu32.lib) link with /MD, so we can't link with /MT.
+# /MD[d] is included in CMAKE_CXX_FLAGS[_*] by default.
+#
+# We also would like to avoid having to build all of Chromium and CEF for our sanity's sake.
 function(BuildCEF)
 	set(CEF_RUNTIME_LIBRARY_FLAG "/MD")
+
+	# Reference: https://magpcss.org/ceforum/viewtopic.php?f=6&t=15482
+	set(USE_SANDBOX OFF)
 
 	add_subdirectory(${CEF_PLATFORM_DIR})
 endfunction(BuildCEF)
@@ -35,6 +44,8 @@ endfunction(IncludeCEF)
 function(LinkCEF TARGET_NAME)
 	target_link_libraries(${TARGET_NAME} "${PROJECT_BINARY_DIR}/extern/${CEF_VERSION_DIR_NAME}/${CEF_PLATFORM_DIR_NAME}/libcef_dll_wrapper/${CEF_CONFIGURATION}/libcef_dll_wrapper.lib")
 	target_link_libraries(${TARGET_NAME} "${CEF_PLATFORM_DIR}/${CEF_CONFIGURATION}/libcef.lib")
+
+	# Sandbox support (linking cef_sandbox.lib) is only possible when your application is built with the /MT flag.
 	#target_link_libraries(${TARGET_NAME} "${CEF_PLATFORM_DIR}/${CEF_CONFIGURATION}/cef_sandbox.lib")
 endfunction(LinkCEF)
 
