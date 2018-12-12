a23533f69f59fb0d3d8b9e9f8a0dd06c2765cd95
Andre Duvoisin <anduv13@gmail.com>
2018-12-12 00:11:07 -0600

    Modularize cmake build system.

---
+++ cmake/CEF.cmake
@@ -0,0 +1,56 @@
+set(CEF_VERSION_DIR_NAME "cef_binary_3.3497.1840.gcd24143")
+set(CEF_PLATFORM_DIR_NAME "windows64")
+
+set(CEF_ROOT_DIR "${EXTERN_DIR}/${CEF_VERSION_DIR_NAME}")
+set(CEF_PLATFORM_DIR "${CEF_ROOT_DIR}/${CEF_PLATFORM_DIR_NAME}")
+
+if(${CE_CONFIGURATION} STREQUAL "Debug")
+	set(CEF_CONFIGURATION "Debug")
+elseif(${CE_CONFIGURATION} STREQUAL "Release")
+	set(CEF_CONFIGURATION "Release")
+endif()
+
+if(${CE_PLATFORM} STREQUAL "Win32")
+	set(CEF_PLATFORM "Win32")
+elseif(${CE_PLATFORM} STREQUAL "x64")
+	set(CEF_PLATFORM "x64")
+endif()
+
+function(BuildCEF)
+	set(CEF_RUNTIME_LIBRARY_FLAG "/MD")
+
+	add_subdirectory(${CEF_PLATFORM_DIR})
+endfunction(BuildCEF)
+
+function(BootstrapCEF TARGET_NAME EXECUTABLE_SUBDIR)
+	IncludeCEF()
+	LinkCEF(${TARGET_NAME})
+	CopyCEFFiles(${EXECUTABLE_SUBDIR})
+endfunction(BootstrapCEF)
+
+function(IncludeCEF)
+	include_directories("${CEF_PLATFORM_DIR}")
+endfunction(IncludeCEF)
+
+function(LinkCEF TARGET_NAME)
+	target_link_libraries(${TARGET_NAME} "${PROJECT_BINARY_DIR}/extern/${CEF_VERSION_DIR_NAME}/${CEF_PLATFORM_DIR_NAME}/libcef_dll_wrapper/${CEF_CONFIGURATION}/libcef_dll_wrapper.lib")
+	target_link_libraries(${TARGET_NAME} "${CEF_PLATFORM_DIR}/${CEF_CONFIGURATION}/libcef.lib")
+	#target_link_libraries(${TARGET_NAME} "${CEF_PLATFORM_DIR}/${CEF_CONFIGURATION}/cef_sandbox.lib")
+endfunction(LinkCEF)
+
+function(CopyCEFFiles EXECUTABLE_SUBDIR)
+	file(GLOB CEF_BINARY_FILES "${CEF_PLATFORM_DIR}/${CEF_CONFIGURATION}/*")
+	file(
+		COPY ${CEF_BINARY_FILES}
+		DESTINATION "${PROJECT_BINARY_DIR}/${EXECUTABLE_SUBDIR}/${CE_CONFIGURATION}"
+		FILES_MATCHING
+			PATTERN "*.dll"
+			PATTERN "*.bin"
+	)
+
+	file(GLOB CEF_RESOURCE_FILES "${CEF_PLATFORM_DIR}/Resources/*")
+	file(
+		COPY ${CEF_RESOURCE_FILES}
+		DESTINATION "${PROJECT_BINARY_DIR}/${EXECUTABLE_SUBDIR}/${CE_CONFIGURATION}"
+	)
+endfunction(CopyCEFFiles)
---
+++ cmake/GLM.cmake
@@ -0,0 +1,9 @@
+set(GLM_ROOT_DIR "${EXTERN_DIR}/glm-0.9.8.5")
+
+function(BootstrapGLM)
+	IncludeGLM()
+endfunction(BootstrapGLM)
+
+function(IncludeGLM)
+	include_directories("${GLM_ROOT_DIR}")
+endfunction(IncludeGLM)
