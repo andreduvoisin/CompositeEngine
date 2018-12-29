fed1a10055664fc3a542a90819acd5589cafc12c
Andre Duvoisin <anduv13@gmail.com>
2018-12-29 17:43:13 -0600

    Add STB as an external project. Clones repository from git.

--- cmake/STBImage.cmake
+++
@@ -1,9 +0,0 @@
-set(STBIMAGE_ROOT_DIR "${EXTERN_DIR}/stb_image-2.16")
-
-function(BootstrapSTBImage)
-	IncludeSTBImage()
-endfunction()
-
-function(IncludeSTBImage)
-	include_directories("${STBIMAGE_ROOT_DIR}")
-endfunction()
