8682ef311de71b9687451619923983257ab6ab98
Andre Duvoisin <anduv13@gmail.com>
2018-12-29 17:59:03 -0600

    Add GLM via external project. Downloads from github.

--- cmake/GLM.cmake
+++ cmake/GLM.cmake
@@ -1,4 +1,25 @@
-set(GLM_ROOT_DIR "${EXTERN_DIR}/glm-0.9.8.5")
+include(ExternalProject)
+
+set(GLM_VERSION "0.9.8.5")
+set(GLM_VERSION_STRING "glm-${GLM_VERSION}")
+
+set(GLM_ROOT_DIR "${EXTERN_DIR}/${GLM_VERSION_STRING}")
+
+function(BuildGLM)
+	ExternalProject_Add(
+		GLM
+		PREFIX ${GLM_VERSION_STRING}
+
+		DOWNLOAD_DIR ${EXTERN_DIR}
+		URL "https://github.com/g-truc/glm/releases/download/${GLM_VERSION}/${GLM_VERSION_STRING}.zip"
+
+		SOURCE_DIR ${GLM_ROOT_DIR}
+
+		CONFIGURE_COMMAND ""
+		BUILD_COMMAND ""
+		INSTALL_COMMAND ""
+	)
+endfunction()
 
 function(BootstrapGLM)
 	IncludeGLM()
