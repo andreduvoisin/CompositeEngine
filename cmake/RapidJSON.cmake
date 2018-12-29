269417d81eb1beef6c6653a35bb852f055931024
Andre Duvoisin <anduv13@gmail.com>
2018-12-29 17:14:04 -0600

    Download RapidJSON from git.

--- cmake/RapidJSON.cmake
+++ cmake/RapidJSON.cmake
@@ -1,4 +1,26 @@
-set(RAPIDJSON_ROOT_DIR "${EXTERN_DIR}/rapidjson-67fac85")
+include(ExternalProject)
+
+set(RAPIDJSON_VERSION "67fac85")
+set(RAPIDJSON_VERSION_STRING "rapidjson-${RAPIDJSON_VERSION}")
+
+set(RAPIDJSON_ROOT_DIR "${EXTERN_DIR}/${RAPIDJSON_VERSION_STRING}")
+
+function(BuildRapidJSON)
+	ExternalProject_Add(
+		RapidJSON
+		PREFIX ${RAPIDJSON_VERSION_STRING}
+
+		DOWNLOAD_DIR ${EXTERN_DIR}
+		GIT_REPOSITORY "https://github.com/Tencent/rapidjson.git"
+		GIT_TAG ${RAPIDJSON_VERSION}
+
+		SOURCE_DIR ${RAPIDJSON_ROOT_DIR}
+
+		CONFIGURE_COMMAND ""
+		BUILD_COMMAND ""
+		INSTALL_COMMAND ""
+	)
+endfunction()
 
 function(BootstrapRapidJSON)
 	IncludeRapidJSON()
