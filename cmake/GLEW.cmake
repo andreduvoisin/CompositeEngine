536e1019f2386b4d42760f67520b0daedf456186
Andre Duvoisin <anduv13@gmail.com>
2019-03-20 09:22:03 -0500

    Convert CMake GLEW to library.

--- cmake/GLEW.cmake
+++ cmake/GLEW.cmake
@@ -27,91 +27,83 @@ if(OS_WINDOWS)
 	endif()
 endif()
 
-function(BuildGLEW)
-	if(OS_WINDOWS)
-		set(CONFIGURE_COMMAND "")
-		set(BUILD_COMMAND
-			MSBuild
-				"${GLEW_MSVC_DIR}/glew.sln"
-				/p:PlatformToolset=v141 # Default: v120
-				/p:Configuration=${CE_CONFIGURATION}
-				/p:Platform=${CE_PLATFORM}
-				/m
-		)
-		set(BUILD_BYPRODUCTS "${GLEW_ROOT_DIR}/lib/${GLEW_CONFIGURATION}/${GLEW_PLATFORM}/glew32s${GLEW_LIB_CONFIGURATION}.lib")
-	elseif(OS_MACOSX)
-		# TODO: Builds release always. Need to pass the CMAKE build var, which changes the lib output, so fix that too.
-		set(CONFIGURE_COMMAND cmake ./cmake)
-		set(BUILD_COMMAND make -j4)
-		set(BUILD_BYPRODUCTS "${GLEW_BUILD_DIR}/lib/libGLEW.a")
-	endif()
-
-	ExternalProject_Add(
-		GLEW
-		PREFIX ${GLEW_VERSION_STRING}
-
-		DOWNLOAD_DIR ${EXTERN_DIR}
-		URL "https://github.com/nigels-com/glew/releases/download/${GLEW_VERSION_STRING}/${GLEW_VERSION_STRING}.tgz"
-
-		SOURCE_DIR ${GLEW_ROOT_DIR}
-		BINARY_DIR ${GLEW_BUILD_DIR}
-
-		CONFIGURE_COMMAND "${CONFIGURE_COMMAND}"
-		BUILD_COMMAND "${BUILD_COMMAND}"
-		INSTALL_COMMAND ""
-
-		BUILD_BYPRODUCTS ${BUILD_BYPRODUCTS}
+if(OS_WINDOWS)
+	set(CONFIGURE_COMMAND "")
+	set(BUILD_COMMAND
+		MSBuild
+			"${GLEW_MSVC_DIR}/glew.sln"
+			/p:PlatformToolset=v141 # Default: v120
+			/p:Configuration=${CE_CONFIGURATION}
+			/p:Platform=${CE_PLATFORM}
+			/m
 	)
-endfunction()
-
-function(BootstrapGLEW TARGET_NAME EXECUTABLE_SUBDIR)
-	IncludeGLEW()
-	LinkGLEW(${TARGET_NAME})
-	CopyGLEWFiles(${EXECUTABLE_SUBDIR})
-endfunction()
-
-function(IncludeGLEW)
-	include_directories("${GLEW_ROOT_DIR}/include")
-endfunction()
-
-function(LinkGLEW TARGET_NAME)
 	if(GLEW_STATIC)
-		if(OS_WINDOWS)
-			set(LIBRARY_STATIC "${GLEW_ROOT_DIR}/lib/${GLEW_CONFIGURATION}/${GLEW_PLATFORM}/glew32s${GLEW_LIB_CONFIGURATION}.lib")
-		elseif(OS_MACOSX)
-			set(LIBRARY_STATIC "${GLEW_BUILD_DIR}/lib/libGLEW.a")
-		endif()
-
-		target_compile_definitions(CompositeEngine PRIVATE GLEW_STATIC)
-		target_link_libraries(CompositeEngine PRIVATE ${LIBRARY_STATIC})
+		set(BUILD_BYPRODUCTS "${GLEW_ROOT_DIR}/lib/${GLEW_CONFIGURATION}/${GLEW_PLATFORM}/glew32s${GLEW_LIB_CONFIGURATION}.lib")
 	elseif(GLEW_DYNAMIC)
-		if(OS_WINDOWS)
-			set(LIBRARY_DYNAMIC "${GLEW_ROOT_DIR}/lib/${GLEW_CONFIGURATION}/${GLEW_PLATFORM}/glew32${GLEW_LIB_CONFIGURATION}.lib")
-		elseif(OS_MACOSX)
-			set(LIBRARY_DYNAMIC "${GLEW_BUILD_DIR}/lib/libGLEW.dylib")
-		endif()
-
-		target_link_libraries(CompositeEngine PRIVATE ${LIBRARY_DYNAMIC})
+		set(BUILD_BYPRODUCTS "${GLEW_ROOT_DIR}/lib/${GLEW_CONFIGURATION}/${GLEW_PLATFORM}/glew32${GLEW_LIB_CONFIGURATION}.lib")
 	endif()
-endfunction()
-
-function(CopyGLEWFiles EXECUTABLE_SUBDIR)
-	if(GLEW_DYNAMIC)
-		if(OS_WINDOWS)
-			set(LIBRARY_DYNAMIC_SRC "${GLEW_ROOT_DIR}/bin/${GLEW_CONFIGURATION}/${GLEW_PLATFORM}/glew32${GLEW_LIB_CONFIGURATION}.dll")
-			set(LIBRARY_DYNAMIC_DST "${PROJECT_BINARY_DIR}/${EXECUTABLE_SUBDIR}/${CE_CONFIGURATION}/glew32${GLEW_LIB_CONFIGURATION}.dll")
-		elseif(OS_MACOSX)
-			set(LIBRARY_DYNAMIC_SRC "${GLEW_BUILD_DIR}/lib/libGLEW.dylib")
-			set(LIBRARY_DYNAMIC_DST "${PROJECT_BINARY_DIR}/engine/${CE_CONFIGURATION}/CompositeEngine.app/Contents/Frameworks/libGLEW.dylib")
-		endif()
-
-		add_custom_command(
-			TARGET ${TARGET_NAME}
-			POST_BUILD
-			COMMAND ${CMAKE_COMMAND} -E copy
-				${LIBRARY_DYNAMIC_SRC}
-				${LIBRARY_DYNAMIC_DST}
-			VERBATIM
-		)
+elseif(OS_MACOSX)
+	# TODO: Builds release always. Need to pass the CMAKE build var, which changes the lib output, so fix that too.
+	set(CONFIGURE_COMMAND cmake ./cmake)
+	set(BUILD_COMMAND make -j4)
+	if(GLEW_STATIC)
+		set(BUILD_BYPRODUCTS "${GLEW_BUILD_DIR}/lib/libGLEW.a")
+	elseif(GLEW_DYNAMIC)
+		set(BUILD_BYPRODUCTS "${GLEW_BUILD_DIR}/lib/libGLEW.dylib")
 	endif()
-endfunction()
+endif()
+
+ExternalProject_Add(
+	GLEWExternal
+	PREFIX ${GLEW_VERSION_STRING}
+
+	DOWNLOAD_DIR ${EXTERN_DIR}
+	URL "https://github.com/nigels-com/glew/releases/download/${GLEW_VERSION_STRING}/${GLEW_VERSION_STRING}.tgz"
+
+	SOURCE_DIR ${GLEW_ROOT_DIR}
+	BINARY_DIR ${GLEW_BUILD_DIR}
+
+	CONFIGURE_COMMAND "${CONFIGURE_COMMAND}"
+	BUILD_COMMAND "${BUILD_COMMAND}"
+	INSTALL_COMMAND ""
+
+	BUILD_BYPRODUCTS ${BUILD_BYPRODUCTS}
+)
+
+add_library(GLEW INTERFACE)
+add_dependencies(GLEW GLEWExternal)
+
+target_include_directories(GLEW INTERFACE "${GLEW_ROOT_DIR}/include")
+
+if(GLEW_STATIC)
+	if(OS_WINDOWS)
+		set(LIBRARY_STATIC "${GLEW_ROOT_DIR}/lib/${GLEW_CONFIGURATION}/${GLEW_PLATFORM}/glew32s${GLEW_LIB_CONFIGURATION}.lib")
+	elseif(OS_MACOSX)
+		set(LIBRARY_STATIC "${GLEW_BUILD_DIR}/lib/libGLEW.a")
+	endif()
+
+	target_compile_definitions(GLEW INTERFACE GLEW_STATIC)
+	target_link_libraries(GLEW INTERFACE ${LIBRARY_STATIC})
+elseif(GLEW_DYNAMIC)
+	if(OS_WINDOWS)
+		set(LIBRARY_DYNAMIC "${GLEW_ROOT_DIR}/lib/${GLEW_CONFIGURATION}/${GLEW_PLATFORM}/glew32${GLEW_LIB_CONFIGURATION}.lib")
+	elseif(OS_MACOSX)
+		set(LIBRARY_DYNAMIC "${GLEW_BUILD_DIR}/lib/libGLEW.dylib")
+	endif()
+
+	target_link_libraries(GLEW INTERFACE ${LIBRARY_DYNAMIC})
+endif()
+
+if(GLEW_DYNAMIC)
+	if(OS_WINDOWS)
+		set(LIBRARY_DYNAMIC_SRC "${GLEW_ROOT_DIR}/bin/${GLEW_CONFIGURATION}/${GLEW_PLATFORM}/glew32${GLEW_LIB_CONFIGURATION}.dll")
+	elseif(OS_MACOSX)
+		set(LIBRARY_DYNAMIC_SRC "${GLEW_BUILD_DIR}/lib/libGLEW.dylib")
+		set(LIBRARY_DYNAMIC_DST "${PROJECT_BINARY_DIR}/engine/${CE_CONFIGURATION}/CompositeEngine.app/Contents/Frameworks/libGLEW.dylib")
+	endif()
+
+	install(
+		FILES "${LIBRARY_DYNAMIC_SRC}"
+		DESTINATION "${CMAKE_INSTALL_PREFIX}"
+	)
+endif()
