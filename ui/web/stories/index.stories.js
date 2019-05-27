bf3b4be6ad31101142c95b03cbe13c06a3eebf32
kevin viglucci <kviglucci@gmail.com>
2019-05-27 16:21:48 -0500

    Update storybook to v5

--- ui/web/stories/index.stories.js
+++ ui/web/stories/index.stories.js
@@ -1,14 +1,16 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
-import { storiesOf } from '@storybook/react';
 import { State, Store } from "@sambego/storybook-state";
+
 import { Welcome } from '@storybook/react/demo';
-import 'font-awesome/css/font-awesome.css';
-import React from 'react';
+
 import AnimationControls from '../src/Components/AnimationControls';
 import Slider from '../src/Components/Slider';
 import Toolbar from '../src/Components/Toolbar';
-import Layout from '../src/Components/Layout';;
+import Layout from '../src/Components/Layout';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
