cb70babafce89e8f34a8ac4f669ae09facea8e27
Andre Duvoisin <anduv13@gmail.com>
2019-06-16 00:04:45 -0500

    clang-format clang-tidy FBXValidator.*

--- asset-converter/3d/fbx/FBXValidator.cpp
+++ asset-converter/3d/fbx/FBXValidator.cpp
@@ -4,103 +4,104 @@
 
 namespace CE
 {
-	FBXValidator::FBXValidator(
-			fbxsdk::FbxManager* manager, 
-			const char* fileName)
-		: manager(manager)
-		, fileName(fileName)
-	{
-		
-	}
+    FBXValidator::FBXValidator(fbxsdk::FbxManager* manager, const char* fileName)
+        : manager(manager)
+        , fileName(fileName)
+    {
+    }
 
-	bool FBXValidator::Validate()
-	{
-		fbxsdk::FbxImporter* importer = fbxsdk::FbxImporter::Create(manager, "");
-		fbxsdk::FbxScene* scene = fbxsdk::FbxScene::Create(manager, "");
+    bool FBXValidator::Validate()
+    {
+        fbxsdk::FbxImporter* importer = fbxsdk::FbxImporter::Create(manager, "");
+        fbxsdk::FbxScene* scene = fbxsdk::FbxScene::Create(manager, "");
 
-		if (!importer->Initialize(fileName, -1, manager->GetIOSettings()))
-		{
-			return false;
-		}
+        if (!importer->Initialize(fileName, -1, manager->GetIOSettings()))
+        {
+            return false;
+        }
 
-		if (!importer->Import(scene))
-		{
-			return false;
-		}
+        if (!importer->Import(scene))
+        {
+            return false;
+        }
 
-		importer->Destroy();
+        importer->Destroy();
 
-		fbxsdk::FbxNode* rootNode = scene->GetRootNode();
-		if (!rootNode)
-		{
-			return false;
-		}
+        fbxsdk::FbxNode* rootNode = scene->GetRootNode();
+        if (rootNode == nullptr)
+        {
+            return false;
+        }
 
-		return ValidateNode(rootNode);
-	}
+        return ValidateNode(rootNode);
+    }
 
-	bool FBXValidator::ValidateNode(fbxsdk::FbxNode* node)
-	{
-		return ValidateGeometry(node)
-			&& ValidateCluster(node)
-			&& ValidateChildren(node);
-	}
+    bool FBXValidator::ValidateNode(fbxsdk::FbxNode* node)
+    {
+        return ValidateGeometry(node) && ValidateCluster(node) && ValidateChildren(node);
+    }
 
-	bool FBXValidator::ValidateGeometry(fbxsdk::FbxNode* node)
-	{
-		fbxsdk::FbxAMatrix geometryTransform(
-			node->GetGeometricTranslation(fbxsdk::FbxNode::EPivotSet::eSourcePivot),
-			node->GetGeometricRotation(fbxsdk::FbxNode::EPivotSet::eSourcePivot),
-			node->GetGeometricScaling(fbxsdk::FbxNode::EPivotSet::eSourcePivot));
+    bool FBXValidator::ValidateGeometry(fbxsdk::FbxNode* node)
+    {
+        fbxsdk::FbxAMatrix geometryTransform(
+                node->GetGeometricTranslation(fbxsdk::FbxNode::EPivotSet::eSourcePivot),
+                node->GetGeometricRotation(fbxsdk::FbxNode::EPivotSet::eSourcePivot),
+                node->GetGeometricScaling(fbxsdk::FbxNode::EPivotSet::eSourcePivot));
 
-		return geometryTransform.IsIdentity();
-	}
+        return geometryTransform.IsIdentity();
+    }
 
-	// TODO: Validate the whole skin. (see ozz-animation)
-	bool FBXValidator::ValidateCluster(fbxsdk::FbxNode* node)
-	{
-		fbxsdk::FbxMesh* mesh = node->GetMesh();
-		if (!mesh)
-		{
-			return true;
-		}
+    // TODO: Validate the whole skin. (see ozz-animation)
+    bool FBXValidator::ValidateCluster(fbxsdk::FbxNode* node)
+    {
+        fbxsdk::FbxMesh* mesh = node->GetMesh();
+        if (mesh == nullptr)
+        {
+            return true;
+        }
 
-		const unsigned deformerCount = mesh->GetDeformerCount();
-		for (unsigned deformerIndex = 0; deformerIndex < deformerCount; ++deformerIndex)
-		{
-			fbxsdk::FbxSkin* skin = reinterpret_cast<fbxsdk::FbxSkin*>(mesh->GetDeformer(deformerIndex, fbxsdk::FbxDeformer::eSkin));
-			if (!skin)
-			{
-				continue;
-			}
+        const int deformerCount = mesh->GetDeformerCount();
+        for (int deformerIndex = 0; deformerIndex < deformerCount; ++deformerIndex)
+        {
+            fbxsdk::FbxDeformer* deformer = mesh->GetDeformer(deformerIndex, fbxsdk::FbxDeformer::eSkin);
+            if (deformer == nullptr)
+            {
+                continue;
+            }
 
-			const unsigned clusterCount = skin->GetClusterCount();
-			for (unsigned clusterIndex = 0; clusterIndex < clusterCount; ++clusterIndex)
-			{
-				fbxsdk::FbxCluster* cluster = skin->GetCluster(clusterIndex);
-				const fbxsdk::FbxCluster::ELinkMode linkMode = cluster->GetLinkMode();
-				if (linkMode != fbxsdk::FbxCluster::ELinkMode::eNormalize)
-				{
-					return false;
-				}
-			}
-		}
+            // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+            auto* skin = reinterpret_cast<fbxsdk::FbxSkin*>(deformer);
+            if (skin == nullptr)
+            {
+                continue;
+            }
 
-		return true;
-	}
+            const int clusterCount = skin->GetClusterCount();
+            for (int clusterIndex = 0; clusterIndex < clusterCount; ++clusterIndex)
+            {
+                fbxsdk::FbxCluster* cluster = skin->GetCluster(clusterIndex);
+                const fbxsdk::FbxCluster::ELinkMode linkMode = cluster->GetLinkMode();
+                if (linkMode != fbxsdk::FbxCluster::ELinkMode::eNormalize)
+                {
+                    return false;
+                }
+            }
+        }
 
+        return true;
+    }
 
-	bool FBXValidator::ValidateChildren(fbxsdk::FbxNode* node)
-	{
-		for (int i = 0; i < node->GetChildCount(); ++i)
-		{
-			fbxsdk::FbxNode* childNode = node->GetChild(i);
-			if (!ValidateNode(childNode))
-			{
-				return false;
-			}
-		}
+    bool FBXValidator::ValidateChildren(fbxsdk::FbxNode* node)
+    {
+        for (int i = 0; i < node->GetChildCount(); ++i)
+        {
+            fbxsdk::FbxNode* childNode = node->GetChild(i);
+            if (!ValidateNode(childNode))
+            {
+                return false;
+            }
+        }
 
-		return true;
-	}
+        return true;
+    }
 }
--- asset-converter/3d/fbx/FBXValidator.h
+++ asset-converter/3d/fbx/FBXValidator.h
@@ -1,33 +1,31 @@
 #ifndef _CE_FBX_VALIDATOR_H_
 #define _CE_FBX_VALIDATOR_H_
 
+// NOLINTNEXTLINE(readability-identifier-naming)
 namespace fbxsdk
 {
-	class FbxManager;
-	class FbxNode;
+    class FbxManager;
+    class FbxNode;
 }
 
 namespace CE
 {
-	class FBXValidator
-	{
-	public:
-		FBXValidator(
-			fbxsdk::FbxManager* manager,
-			const char* fileName);
+    class FBXValidator
+    {
+    public:
+        FBXValidator(fbxsdk::FbxManager* manager, const char* fileName);
 
-		bool Validate();
+        bool Validate();
 
-	private:
-		bool ValidateNode(fbxsdk::FbxNode* node);
-		bool ValidateGeometry(fbxsdk::FbxNode* node);
-		bool ValidateCluster(fbxsdk::FbxNode* node);
-		bool ValidateChildren(fbxsdk::FbxNode* node);
+    private:
+        bool ValidateNode(fbxsdk::FbxNode* node);
+        bool ValidateGeometry(fbxsdk::FbxNode* node);
+        bool ValidateCluster(fbxsdk::FbxNode* node);
+        bool ValidateChildren(fbxsdk::FbxNode* node);
 
-	private:
-		fbxsdk::FbxManager* manager;
-		const char* fileName;
-	};
+        fbxsdk::FbxManager* manager;
+        const char* fileName;
+    };
 }
 
 #endif // _CE_FBX_VALIDATOR_H_
