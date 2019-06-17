d5c9dbde666547a56922d71ef0893ff8f4222641
Andre Duvoisin <anduv13@gmail.com>
2019-06-17 02:03:59 -0500

    Bump GSL version to latest master. Disable RTTI and exceptions.

--- cmake/GSL.cmake
+++ cmake/GSL.cmake
@@ -1,6 +1,6 @@
 include(ExternalProject)
 
-set(GSL_VERSION "v2.0.0")
+set(GSL_VERSION "1212beae777dba02c230ece8c0c0ec12790047ea")
 set(GSL_VERSION_STRING "gsl-${GSL_VERSION}")
 
 set(GSL_ROOT_DIR "${EXTERN_DIR}/${GSL_VERSION_STRING}")
@@ -10,7 +10,8 @@ ExternalProject_Add(
 	PREFIX ${GSL_VERSION_STRING}
 
 	DOWNLOAD_DIR ${EXTERN_DIR}
-	URL "https://github.com/microsoft/GSL/archive/${GSL_VERSION}.zip"
+	GIT_REPOSITORY "https://github.com/microsoft/GSL.git"
+	GIT_TAG ${GSL_VERSION}
 
 	SOURCE_DIR ${GSL_ROOT_DIR}
 
