ae7c79401dffdeffb10a63a3815572797cca016f
Andre Duvoisin <anduv13@gmail.com>
2019-06-20 00:30:11 -0500

    clang-tidy clang-format FBXSkeletonImporter

--- asset-converter/3d/fbx/FBXSkeletonImporter.cpp
+++ asset-converter/3d/fbx/FBXSkeletonImporter.cpp
@@ -4,179 +4,185 @@
 #include "graphics/skeleton/Skeleton.h"
 
 #include <fbxsdk.h>
+#include <iostream>
 
 namespace CE
 {
-	FBXSkeletonImporter::FBXSkeletonImporter(
-			fbxsdk::FbxManager* fbxManager,
-			const char* szFileName,
-			Skeleton* outSkeleton)
-		: m_fbxManager(fbxManager)
-		, m_szFileName(szFileName)
-		, m_outSkeleton(outSkeleton)
-	{
+    FBXSkeletonImporter::FBXSkeletonImporter(
+            fbxsdk::FbxManager* fbxManager,
+            const char* fileName,
+            Skeleton* outSkeleton)
+        : fbxManager(fbxManager)
+        , fileName(fileName)
+        , outSkeleton(outSkeleton)
+    {
+    }
 
-	}
+    bool FBXSkeletonImporter::LoadSkeleton()
+    {
+        FBXValidator validator(fbxManager, fileName);
+        if (!validator.Validate())
+        {
+            return false;
+        }
 
-	bool FBXSkeletonImporter::LoadSkeleton()
-	{
-		FBXValidator validator(m_fbxManager, m_szFileName);
-		if (!validator.Validate())
-		{
-			return false;
-		}
+        fbxsdk::FbxImporter* importer = fbxsdk::FbxImporter::Create(fbxManager, "");
+        fbxsdk::FbxScene* fbxScene = fbxsdk::FbxScene::Create(fbxManager, "");
 
-		fbxsdk::FbxImporter* importer = fbxsdk::FbxImporter::Create(m_fbxManager, "");
-		fbxsdk::FbxScene* fbxScene = fbxsdk::FbxScene::Create(m_fbxManager, "");
+        if (!importer->Initialize(fileName, -1, fbxManager->GetIOSettings()))
+        {
+            return false;
+        }
 
-		if (!importer->Initialize(m_szFileName, -1, m_fbxManager->GetIOSettings()))
-		{
-			return false;
-		}
+        if (!importer->Import(fbxScene))
+        {
+            return false;
+        }
 
-		if (!importer->Import(fbxScene))
-		{
-			return false;
-		}
+        importer->Destroy();
 
-		importer->Destroy();
+        fbxsdk::FbxNode* rootNode = fbxScene->GetRootNode();
+        if (rootNode == nullptr)
+        {
+            return false;
+        }
 
-		fbxsdk::FbxNode* rootNode = fbxScene->GetRootNode();
-		if (!rootNode)
-		{
-			return false;
-		}
+        ProcessSkeletonHierarchy(rootNode, fbxScene);
 
-		ProcessSkeletonHierarchy(rootNode, fbxScene);
+        return true;
+    }
 
-		return true;
-	}
+    void FBXSkeletonImporter::ProcessSkeletonHierarchy(fbxsdk::FbxNode* inRootNode, fbxsdk::FbxScene* pFbxScene)
+    {
+        for (int childIndex = 0; childIndex < inRootNode->GetChildCount(); ++childIndex)
+        {
+            fbxsdk::FbxNode* currNode = inRootNode->GetChild(childIndex);
+            ProcessSkeletonHierarchyRecursively(currNode, 0, -1);
+        }
 
-	void FBXSkeletonImporter::ProcessSkeletonHierarchy(fbxsdk::FbxNode* inRootNode, fbxsdk::FbxScene* pFbxScene)
-	{
-		for (int childIndex = 0; childIndex < inRootNode->GetChildCount(); ++childIndex)
-		{
-			fbxsdk::FbxNode* currNode = inRootNode->GetChild(childIndex);
-			ProcessSkeletonHierarchyRecursively(currNode, 0, -1);
-		}
+        std::vector<bool> isJointUsed;
+        isJointUsed.resize(outSkeleton->joints.size(), false);
 
+        const int meshCount = pFbxScene->GetSrcObjectCount<fbxsdk::FbxMesh>();
+        for (int meshIndex = 0; meshIndex < meshCount; meshIndex++)
+        {
+            auto* currMesh = pFbxScene->GetSrcObject<fbxsdk::FbxMesh>(meshIndex);
 
-		std::vector<bool> isJointUsed;
-		isJointUsed.resize(m_outSkeleton->joints.size(), false);
+            int numOfDeformers = currMesh->GetDeformerCount();
 
-		const int meshCount = pFbxScene->GetSrcObjectCount<fbxsdk::FbxMesh>();
-		for (int meshIndex = 0; meshIndex < meshCount; meshIndex++)
-		{
-			fbxsdk::FbxMesh* currMesh = pFbxScene->GetSrcObject<fbxsdk::FbxMesh>(meshIndex);
+            for (int deformerIndex = 0; deformerIndex < numOfDeformers; ++deformerIndex)
+            {
+                // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+                auto* currSkin = reinterpret_cast<fbxsdk::FbxSkin*>(
+                        currMesh->GetDeformer(deformerIndex, fbxsdk::FbxDeformer::eSkin));
+                if (currSkin == nullptr)
+                {
+                    continue;
+                }
 
-			unsigned int numOfDeformers = currMesh->GetDeformerCount();
+                int numOfClusters = currSkin->GetClusterCount();
+                for (int clusterIndex = 0; clusterIndex < numOfClusters; ++clusterIndex)
+                {
+                    fbxsdk::FbxCluster* currCluster = currSkin->GetCluster(clusterIndex);
+                    std::string currJointName = currCluster->GetLink()->GetName();
 
-			for (unsigned deformerIndex = 0; deformerIndex < numOfDeformers; ++deformerIndex)
-			{
-				fbxsdk::FbxSkin* currSkin = reinterpret_cast<fbxsdk::FbxSkin*>(currMesh->GetDeformer(deformerIndex, fbxsdk::FbxDeformer::eSkin));
-				if (!currSkin)
-				{
-					continue;
-				}
+                    size_t currJointIndex = -1;
+                    for (size_t i = 0; i < outSkeleton->joints.size(); ++i)
+                    {
+                        if (outSkeleton->joints[i].name == currJointName)
+                        {
+                            currJointIndex = i;
+                            break;
+                        }
+                    }
 
-				unsigned numOfClusters = currSkin->GetClusterCount();
-				for (unsigned clusterIndex = 0; clusterIndex < numOfClusters; ++clusterIndex)
-				{
-					fbxsdk::FbxCluster* currCluster = currSkin->GetCluster(clusterIndex);
-					std::string currJointName = currCluster->GetLink()->GetName();
+                    // TODO: Swap to ozz-animation way of doing this?
+                    if (currJointIndex == -1)
+                    {
+                        std::cout << "Couldn't find joint index.\n";
+                        continue; // should this break? or return even?
+                    }
 
-					unsigned int currJointIndex = -1;
-					for (unsigned i = 0; i < m_outSkeleton->joints.size(); ++i)
-					{
-						if (m_outSkeleton->joints[i].name == currJointName)
-						{
-							currJointIndex = i;
-							break;
-						}
-					}
+                    // TransformLink refers to global initial transform of the link node.
+                    // TransformLink is the global transform of the bone(link) at the binding moment.
+                    // In the future, if local transform is needed (instead of global), you must multiply up the chain
+                    // with inverses.
+                    fbxsdk::FbxAMatrix transformLinkMatrix;
+                    transformLinkMatrix = currCluster->GetTransformLinkMatrix(transformLinkMatrix);
 
-					// TODO: Swap to ozz-animation way of doing this?
-					if (currJointIndex == -1)
-					{
-						printf("Couldn't find joint index.\n");
-						continue; // should this break? or return even?
-					}
+                    const fbxsdk::FbxAMatrix inverseBindPose = transformLinkMatrix.Inverse();
 
-					// TransformLink refers to global initial transform of the link node.
-					// TransformLink is the global transform of the bone(link) at the binding moment.
-					// In the future, if local transform is needed (instead of global), you must multiply up the chain with inverses.
-					fbxsdk::FbxAMatrix transform_link_matrix;
-					transform_link_matrix = currCluster->GetTransformLinkMatrix(transform_link_matrix);
+                    const int totalMatrixComponents = 16;
+                    for (int i = 0; i < totalMatrixComponents; ++i)
+                    {
+                        outSkeleton->joints[currJointIndex].inverseBindPose[i / 4][i % 4] =
+                                (float) inverseBindPose.Get(i / 4, i % 4);
+                    }
 
-					const fbxsdk::FbxAMatrix inverse_bind_pose = transform_link_matrix.Inverse();
+                    unsigned int numOfIndices = currCluster->GetControlPointIndicesCount();
+                    if (numOfIndices > 0)
+                    {
+                        isJointUsed[currJointIndex] = true;
+                    }
+                }
+            }
+        }
 
-					for (unsigned i = 0; i < 16; ++i)
-					{
-						m_outSkeleton->joints[currJointIndex].inverseBindPose[i / 4][i % 4] = (float) inverse_bind_pose.Get(i / 4, i % 4);
-					}
+        size_t numJointsRemoved = 0;
+        for (size_t i = 0; i < isJointUsed.size(); ++i)
+        {
+            if (!isJointUsed[i] && !JointHasChild(i - numJointsRemoved))
+            {
+                RemoveJoint(i - numJointsRemoved);
+                ++numJointsRemoved;
+            }
+        }
+    }
 
-					unsigned int numOfIndices = currCluster->GetControlPointIndicesCount();
-					if (numOfIndices > 0)
-					{
-						isJointUsed[currJointIndex] = true;
-					}
-				}
-			}
-		}
+    bool FBXSkeletonImporter::JointHasChild(size_t index)
+    {
+        for (const auto& joint : outSkeleton->joints)
+        {
+            if (joint.parentIndex == index)
+            {
+                return true;
+            }
+        }
 
-		size_t numJointsRemoved = 0;
-		for (size_t i = 0; i < isJointUsed.size(); ++i)
-		{
-			if (!isJointUsed[i] && !JointHasChild(i - numJointsRemoved))
-			{
-				RemoveJoint(i - numJointsRemoved);
-				++numJointsRemoved;
-			}
-		}
-	}
+        return false;
+    }
 
-	bool FBXSkeletonImporter::JointHasChild(size_t index)
-	{
-		for (size_t i = 0; i < m_outSkeleton->joints.size(); ++i)
-		{
-			if (m_outSkeleton->joints[i].parentIndex == index)
-			{
-				return true;
-			}
-		}
+    void FBXSkeletonImporter::RemoveJoint(size_t index)
+    {
+        outSkeleton->joints.erase(outSkeleton->joints.begin() + index);
 
-		return false;
-	}
+        for (size_t i = index; i < outSkeleton->joints.size(); ++i)
+        {
+            if (outSkeleton->joints[i].parentIndex > static_cast<short>(index))
+            {
+                --outSkeleton->joints[i].parentIndex;
+            }
+        }
+    }
 
-	void FBXSkeletonImporter::RemoveJoint(size_t index)
-	{
-		m_outSkeleton->joints.erase(m_outSkeleton->joints.begin() + index);
+    void FBXSkeletonImporter::ProcessSkeletonHierarchyRecursively(
+            fbxsdk::FbxNode* inNode,
+            size_t myIndex,
+            size_t inParentIndex)
+    {
+        if (inNode->GetNodeAttribute() != nullptr
+            && inNode->GetNodeAttribute()->GetAttributeType() == fbxsdk::FbxNodeAttribute::eSkeleton)
+        {
+            Joint currJoint;
+            currJoint.parentIndex = (short) inParentIndex;
+            currJoint.name = inNode->GetName();
+            currJoint.inverseBindPose = glm::mat4(1.F);
 
-		for (size_t i = index; i < m_outSkeleton->joints.size(); ++i)
-		{
-			if (m_outSkeleton->joints[i].parentIndex > static_cast<short>(index))
-			{
-				--m_outSkeleton->joints[i].parentIndex;
-			}
-		}
-	}
-
-	void FBXSkeletonImporter::ProcessSkeletonHierarchyRecursively(fbxsdk::FbxNode* inNode, size_t myIndex, size_t inParentIndex)
-	{
-		if (inNode->GetNodeAttribute()
-			&& inNode->GetNodeAttribute()->GetAttributeType()
-			&& inNode->GetNodeAttribute()->GetAttributeType() == fbxsdk::FbxNodeAttribute::eSkeleton)
-		{
-			Joint currJoint;
-			currJoint.parentIndex = (short) inParentIndex;
-			currJoint.name = inNode->GetName();
-			currJoint.inverseBindPose = glm::mat4(1.f);
-
-			m_outSkeleton->joints.push_back(currJoint);
-		}
-		for (int i = 0; i < inNode->GetChildCount(); i++)
-		{
-			ProcessSkeletonHierarchyRecursively(inNode->GetChild(i), m_outSkeleton->joints.size(), myIndex);
-		}
-	}
+            outSkeleton->joints.push_back(currJoint);
+        }
+        for (int i = 0; i < inNode->GetChildCount(); i++)
+        {
+            ProcessSkeletonHierarchyRecursively(inNode->GetChild(i), outSkeleton->joints.size(), myIndex);
+        }
+    }
 }
--- asset-converter/3d/fbx/FBXSkeletonImporter.h
+++ asset-converter/3d/fbx/FBXSkeletonImporter.h
@@ -3,38 +3,36 @@
 
 #include <cstddef>
 
+// NOLINTNEXTLINE(readability-identifier-naming)
 namespace fbxsdk
 {
-	class FbxManager;
-	class FbxNode;
-	class FbxScene;
+    class FbxManager;
+    class FbxNode;
+    class FbxScene;
 }
 
 namespace CE
 {
-	struct Skeleton;
+    struct Skeleton;
 
-	class FBXSkeletonImporter
-	{
-	public:
-		FBXSkeletonImporter(
-			fbxsdk::FbxManager* fbxManager,
-			const char* szFileName,
-			Skeleton* outSkeleton);
+    class FBXSkeletonImporter
+    {
+    public:
+        FBXSkeletonImporter(fbxsdk::FbxManager* fbxManager, const char* fileName, Skeleton* outSkeleton);
 
-		bool LoadSkeleton();
+        bool LoadSkeleton();
 
-	private:
-		void ProcessSkeletonHierarchy(fbxsdk::FbxNode* inRootNode, fbxsdk::FbxScene* pFbxScene);
-		void ProcessSkeletonHierarchyRecursively(fbxsdk::FbxNode* inNode, size_t myIndex, size_t inParentIndex);
-		bool JointHasChild(size_t index);
-		void RemoveJoint(size_t index);
+    private:
+        void ProcessSkeletonHierarchy(fbxsdk::FbxNode* inRootNode, fbxsdk::FbxScene* pFbxScene);
+        void ProcessSkeletonHierarchyRecursively(fbxsdk::FbxNode* inNode, size_t myIndex, size_t inParentIndex);
+        bool JointHasChild(size_t index);
+        void RemoveJoint(size_t index);
 
-	private:
-		fbxsdk::FbxManager* m_fbxManager;
-		const char* m_szFileName;
-		Skeleton* m_outSkeleton;
-	};
+    private:
+        fbxsdk::FbxManager* fbxManager;
+        const char* fileName;
+        Skeleton* outSkeleton;
+    };
 }
 
 #endif // _CE_FBX_SKELETON_IMPORTER_H_
