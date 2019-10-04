c2effcd4879c729f694bfbe7b9cdad61475ff2cb
Andre Duvoisin <anduv13@gmail.com>
2019-10-04 00:18:36 -0500

    lint FBXMeshImporter.h

--- asset-converter/3d/fbx/FBXMeshImporter.h
+++ asset-converter/3d/fbx/FBXMeshImporter.h
@@ -2,48 +2,50 @@
 #define _CE_FBX_MESH_IMPORTER_H_
 
 #include <unordered_map>
+
 #include <vector>
 
+// NOLINTNEXTLINE(readability-identifier-naming)
 namespace fbxsdk
 {
-	class FbxManager;
-	class FbxMesh;
-	class FbxNode;
-	class FbxScene;
-	class FbxSurfaceMaterial;
+    class FbxManager;
+    class FbxMesh;
+    class FbxNode;
+    class FbxScene;
+    class FbxSurfaceMaterial;
 }
 
 namespace CE
 {
-	struct Mesh;
-	typedef std::vector<Mesh> Meshes;
-	struct Skeleton;
+    struct Mesh;
+    using Meshes = std::vector<Mesh>;
+    struct Skeleton;
 
-	class FBXMeshImporter
-	{
-	public:
-		FBXMeshImporter(
-			fbxsdk::FbxManager* fbxManager,
-			const char* fileName,
-			const Skeleton& skeleton,
-			Meshes* outMeshes);
+    class FBXMeshImporter
+    {
+    public:
+        FBXMeshImporter(
+                fbxsdk::FbxManager* fbxManager,
+                const char* fileName,
+                const Skeleton& skeleton,
+                Meshes* outMeshes);
 
-		bool LoadMeshes();
+        bool LoadMeshes();
 
-	private:
-		void ParseNodes(fbxsdk::FbxNode* rootNode, fbxsdk::FbxScene* scene);
-		void ProcessVertices(Mesh& currentMesh, fbxsdk::FbxMesh* mesh);
-		void ProcessMaterialTexture(Mesh& currentMesh, fbxsdk::FbxSurfaceMaterial* material);
-		void ProcessSkinnedMesh(Mesh& currentMesh, fbxsdk::FbxNode* node, fbxsdk::FbxScene* scene);
+    private:
+        void ParseNodes(fbxsdk::FbxNode* rootNode, fbxsdk::FbxScene* scene);
+        void ProcessVertices(Mesh& currentMesh, fbxsdk::FbxMesh* mesh);
+        void ProcessMaterialTexture(Mesh& currentMesh, fbxsdk::FbxSurfaceMaterial* material);
+        void ProcessSkinnedMesh(Mesh& currentMesh, fbxsdk::FbxNode* node, fbxsdk::FbxScene* scene);
 
-	private:
-		fbxsdk::FbxManager* fbxManager;
-		const char* fileName;
-		const Skeleton& skeleton;
-		Meshes* outMeshes;
+    private:
+        fbxsdk::FbxManager* fbxManager;
+        const char* fileName;
+        const Skeleton& skeleton;
+        Meshes* outMeshes;
 
-		std::unordered_map<int, std::vector<int>> controlPointToVertices;
-	};
+        std::unordered_map<int, std::vector<int>> controlPointToVertices;
+    };
 }
 
 #endif // _CE_FBX_MESH_IMPORTER_H_
