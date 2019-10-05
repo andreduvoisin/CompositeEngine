954373b02819d7093812efd8e60937c81c00bf51
Andre Duvoisin <anduv13@gmail.com>
2019-10-04 23:02:51 -0500

    lint uiexternalmessagepump.h

--- ui/cef/browser/UIExternalMessagePump.h
+++ ui/cef/browser/UIExternalMessagePump.h
@@ -12,30 +12,35 @@
 class UIExternalMessagePump
 {
 public:
-	UIExternalMessagePump();
-	~UIExternalMessagePump();
+    UIExternalMessagePump();
+    ~UIExternalMessagePump();
 
-	void OnScheduleMessagePumpWork(uint32_t delayMillis);
-	void ProcessEvent(const SDL_Event& event);
+    UIExternalMessagePump(const UIExternalMessagePump&) = delete;
+    UIExternalMessagePump(UIExternalMessagePump&&) = delete;
+    UIExternalMessagePump& operator=(const UIExternalMessagePump&) = delete;
+    UIExternalMessagePump& operator=(UIExternalMessagePump&&) = delete;
 
-	void Shutdown();
+    void OnScheduleMessagePumpWork(uint32_t delayMillis);
+    void ProcessEvent(const SDL_Event& event);
+
+    void Shutdown();
 
 private:
-	static uint32_t TimerCallback(uint32_t intervalMillis, void* param);
-	static void PushEvent(uint32_t type, void* data1, void* data2);
+    static uint32_t TimerCallback(uint32_t intervalMillis, void* param);
+    static void PushEvent(uint32_t type, void* data1, void* data2);
 
-	bool IsTimerPending();
-	void SetTimer(uint32_t delayMillis);
-	void KillTimer();
-	void OnTimerTimeout();
+    bool IsTimerPending();
+    void SetTimer(uint32_t delayMillis);
+    void KillTimer();
+    void OnTimerTimeout();
 
-	void OnScheduleWork(uint32_t delayMillis);
-	void DoWork();
-	bool PerformMessageLoopWork();
+    void OnScheduleWork(uint32_t delayMillis);
+    void DoWork();
+    bool PerformMessageLoopWork();
 
-	SDL_TimerID timerId;
-	bool active;
-	bool reentrancyDetected;
+    SDL_TimerID timerId;
+    bool active;
+    bool reentrancyDetected;
 };
 
 #endif //_CE_UI_EXTERNAL_MESSAGE_PUMP_H_
