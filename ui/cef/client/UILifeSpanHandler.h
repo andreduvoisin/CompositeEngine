7f9920755158799a67a68b9ec2559c63cc0c265a
Andre Duvoisin <anduv13@gmail.com>
2019-10-07 23:42:30 -0500

    lint uilifespanhandler.h

--- ui/cef/client/UILifeSpanHandler.h
+++ ui/cef/client/UILifeSpanHandler.h
@@ -7,16 +7,16 @@
 class UILifeSpanHandler : public CefLifeSpanHandler
 {
 public:
-	UILifeSpanHandler(CefRefPtr<CefMessageRouterBrowserSide> messageRouterBrowserSide);
+    explicit UILifeSpanHandler(CefRefPtr<CefMessageRouterBrowserSide> messageRouterBrowserSide);
 
-	// CefLifeSpanHandler Interface
-	void OnBeforeClose(CefRefPtr<CefBrowser> browser) override;
+    // CefLifeSpanHandler Interface
+    void OnBeforeClose(CefRefPtr<CefBrowser> browser) override;
 
 private:
-	CefRefPtr<CefMessageRouterBrowserSide> messageRouterBrowserSide;
+    CefRefPtr<CefMessageRouterBrowserSide> messageRouterBrowserSide;
 
-	// IMPLEMENT_* macros set access modifiers, so they must come last.
-	IMPLEMENT_REFCOUNTING(UILifeSpanHandler);
+    // IMPLEMENT_* macros set access modifiers, so they must come last.
+    IMPLEMENT_REFCOUNTING(UILifeSpanHandler);
 };
 
 #endif //_CE_UI_LIFE_SPAN_HANDLER_H_
