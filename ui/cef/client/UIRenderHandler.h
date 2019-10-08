61d014eed38e5b90fc6818fe4fd6f9f020ab874b
Andre Duvoisin <anduv13@gmail.com>
2019-10-07 23:48:01 -0500

    lint uirenderhandler.h

--- ui/cef/client/UIRenderHandler.h
+++ ui/cef/client/UIRenderHandler.h
@@ -6,41 +6,50 @@
 class UIRenderHandler : public CefRenderHandler
 {
 public:
-	UIRenderHandler(unsigned width, unsigned height);
+    UIRenderHandler(unsigned width, unsigned height);
 
-	void Resize(unsigned width, unsigned height);
-	void Render();
+    void Resize(unsigned width, unsigned height);
+    void Render();
 
-	// CefRenderHandler Interface
-	void GetViewRect(CefRefPtr<CefBrowser> browser, CefRect& rect) override;
-	void OnPopupShow(CefRefPtr<CefBrowser> browser, bool show) override;
-	void OnPopupSize(CefRefPtr<CefBrowser> browser, const CefRect& rect) override;
-	void OnPaint(
-		CefRefPtr<CefBrowser> browser,
-		PaintElementType type,
-		const RectList& dirtyRects,
-		const void* buffer,
-		int width,
-		int height) override;
+    // CefRenderHandler Interface
+    void GetViewRect(CefRefPtr<CefBrowser> browser, CefRect& rect) override;
+    void OnPopupShow(CefRefPtr<CefBrowser> browser, bool show) override;
+    void OnPopupSize(CefRefPtr<CefBrowser> browser, const CefRect& rect) override;
+    void OnPaint(
+            CefRefPtr<CefBrowser> browser,
+            PaintElementType type,
+            const RectList& dirtyRects,
+            const void* buffer,
+            int width,
+            int height) override;
 
-	// TODO: Remove and render with Render().
-	char* GetViewBuffer() const { return viewBuffer; }
-	char* GetPopupBuffer() const { return popupBuffer; }
-	const CefRect& GetPopupRect() const { return popupRect; }
+    // TODO: Remove and render with Render().
+    char* GetViewBuffer() const
+    {
+        return viewBuffer;
+    }
+    char* GetPopupBuffer() const
+    {
+        return popupBuffer;
+    }
+    const CefRect& GetPopupRect() const
+    {
+        return popupRect;
+    }
 
 private:
-	unsigned width, height;
+    unsigned width, height;
 
-	// From the CefRenderHandler OnPaint documentation:
-	// |buffer| will be |width|*|height|*4 bytes in size and
-	// represents a BGRA image with an upper-left origin.
-	char* viewBuffer;
-	char* popupBuffer;
+    // From the CefRenderHandler OnPaint documentation:
+    // |buffer| will be |width|*|height|*4 bytes in size and
+    // represents a BGRA image with an upper-left origin.
+    char* viewBuffer;
+    char* popupBuffer;
 
-	CefRect popupRect;
+    CefRect popupRect;
 
-	// IMPLEMENT_* macros set access modifiers, so they must come last.
-	IMPLEMENT_REFCOUNTING(UIRenderHandler);
+    // IMPLEMENT_* macros set access modifiers, so they must come last.
+    IMPLEMENT_REFCOUNTING(UIRenderHandler);
 };
 
 #endif //_CE_UI_RENDER_HANDLER_H_
