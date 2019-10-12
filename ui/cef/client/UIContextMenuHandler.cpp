3e72cce397a573fc664b7cbe577a09b898562734
Andre Duvoisin <anduv13@gmail.com>
2019-10-12 18:05:44 -0500

    lint: ui/cef/client/UIContextMenuHandler.cpp

--- ui/cef/client/UIContextMenuHandler.cpp
+++ ui/cef/client/UIContextMenuHandler.cpp
@@ -1,11 +1,11 @@
 #include "UIContextMenuHandler.h"
 
 void UIContextMenuHandler::OnBeforeContextMenu(
-		CefRefPtr<CefBrowser> browser,
-		CefRefPtr<CefFrame> frame,
-		CefRefPtr<CefContextMenuParams> params,
-		CefRefPtr<CefMenuModel> model)
+        CefRefPtr<CefBrowser> /*browser*/,
+        CefRefPtr<CefFrame> /*frame*/,
+        CefRefPtr<CefContextMenuParams> /*params*/,
+        CefRefPtr<CefMenuModel> model)
 {
-	// Disable the default right-click context menu.
-	model->Clear();
+    // Disable the default right-click context menu.
+    model->Clear();
 }
