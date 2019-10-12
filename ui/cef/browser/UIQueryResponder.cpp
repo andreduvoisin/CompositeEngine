b9dc903b4febe8f7d55bfeef58ebbcec716771b0
Andre Duvoisin <anduv13@gmail.com>
2019-10-12 17:45:26 -0500

    lint uiqueryresponder

--- ui/cef/browser/UIQueryResponder.cpp
+++ ui/cef/browser/UIQueryResponder.cpp
@@ -2,100 +2,101 @@
 
 #include "event/core/EventSystem.h"
 
+#include <array>
+
 UIQueryResponder::UIQueryResponder(EventSystem* eventSystem)
 {
-	EventType types [] = {
-		EventType::PAUSE_STATE,
-		EventType::ANIMATION_STATE,
-		EventType::FPS_STATE
-	};
+    std::array<EventType, 3> types = {EventType::PAUSE_STATE, EventType::ANIMATION_STATE, EventType::FPS_STATE};
 
-	for (EventType type : types)
-	{
-		eventSystem->RegisterListener(this, type);
-		eventToQueries[type] = std::list<UIQuery>();
-	}
+    for (EventType type : types)
+    {
+        eventSystem->RegisterListener(this, type);
+        eventToQueries[type] = std::list<UIQuery>();
+    }
 }
 
 void UIQueryResponder::OnEvent(const Event& event)
 {
-	BroadcastEvent(event);
+    BroadcastEvent(event);
 }
 
 void UIQueryResponder::AddQuery(EventType type, const UIQuery& query)
 {
-	switch (type)
-	{
-		case EventType::REQUEST_PAUSE_STATE:
-		{
-			RegisterQueryForEvent(EventType::PAUSE_STATE, query);
-			break;
-		}
+    switch (type)
+    {
+        case EventType::REQUEST_PAUSE_STATE:
+        {
+            RegisterQueryForEvent(EventType::PAUSE_STATE, query);
+            break;
+        }
 
-		case EventType::REQUEST_ANIMATION_STATE:
-		{
-			RegisterQueryForEvent(EventType::ANIMATION_STATE, query);
-			break;
-		}
+        case EventType::REQUEST_ANIMATION_STATE:
+        {
+            RegisterQueryForEvent(EventType::ANIMATION_STATE, query);
+            break;
+        }
 
-		case EventType::REQUEST_FPS_STATE:
-		{
-			RegisterQueryForEvent(EventType::FPS_STATE, query);
-			break;
-		}
-	}
+        case EventType::REQUEST_FPS_STATE:
+        {
+            RegisterQueryForEvent(EventType::FPS_STATE, query);
+            break;
+        }
+
+        default:
+            break;
+    }
 }
 
 void UIQueryResponder::RemoveQuery(int64_t queryId)
 {
-	for (auto& eventToQueryIt : eventToQueries)
-	{
-		std::list<UIQuery>& queries = eventToQueryIt.second;
+    for (auto& eventToQueryIt : eventToQueries)
+    {
+        std::list<UIQuery>& queries = eventToQueryIt.second;
 
-		for (auto queriesIt = queries.begin(); queriesIt != queries.end(); ++queriesIt)
-		{
-			if (queriesIt->queryId == queryId)
-			{
-				queries.erase(queriesIt);
-				break;
-			}
-		}
-	}
+        for (auto queriesIt = queries.begin(); queriesIt != queries.end(); ++queriesIt)
+        {
+            if (queriesIt->queryId == queryId)
+            {
+                queries.erase(queriesIt);
+                break;
+            }
+        }
+    }
 }
 
 void UIQueryResponder::RegisterQueryForEvent(EventType type, const UIQuery& query)
 {
-	eventToQueries[type].push_back(query);
+    eventToQueries[type].push_back(query);
 }
 
 void UIQueryResponder::BroadcastEvent(const Event& event)
 {
-	auto it = eventToQueries.find(event.type);
-	if (it == eventToQueries.end())
-	{
-		return;
-	}
+    auto it = eventToQueries.find(event.type);
+    if (it == eventToQueries.end())
+    {
+        return;
+    }
 
-	std::list<UIQuery>& queries = it->second;
-	if (queries.empty())
-	{
-		return;
-	}
+    std::list<UIQuery>& queries = it->second;
+    if (queries.empty())
+    {
+        return;
+    }
 
-	std::string buffer = event.Serialize();
+    std::string buffer = event.Serialize();
 
-	auto queriesIt = queries.begin();
-	while (queriesIt != queries.end())
-	{
-		queriesIt->callback->Success(buffer);
+    auto queriesIt = queries.begin();
+    while (queriesIt != queries.end())
+    {
+        queriesIt->callback->Success(buffer);
 
-		if (queriesIt->persistent)
-		{
-			++queriesIt;
-		}
-		else
-		{
-			queriesIt = queries.erase(queriesIt);
-		}
-	}
+        if (queriesIt->persistent)
+        {
+            ++queriesIt;
+        }
+        else
+        {
+            queriesIt = queries.erase(queriesIt);
+        }
+    }
 }
