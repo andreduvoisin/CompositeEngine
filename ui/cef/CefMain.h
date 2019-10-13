378897c24d452d43fef8c40f9ec3237884429821
Andre Duvoisin <anduv13@gmail.com>
2019-10-12 22:47:14 -0500

    lint: ui/cef/CefMain.h

--- ui/cef/CefMain.h
+++ ui/cef/CefMain.h
@@ -2,7 +2,6 @@
 #define _CE_CEF_MAIN_H_
 
 #include "event/core/EventListener.h"
-
 #include "include/cef_base.h"
 
 class EventSystem;
@@ -17,18 +16,13 @@ struct SDL_Window;
 
 namespace CE
 {
-	class CefMain : public EventListener
-	{
-	public:
-		CefMain(
-            EventSystem* eventSystem,
-            SDL_Window* window);
+    class CefMain : public EventListener
+    {
+    public:
+        CefMain(EventSystem* eventSystem, SDL_Window* window);
 
-        bool StartCef(
-            int argc,
-            char* argv[],
-            uint32_t screenWidth,
-            uint32_t screenHeight);
+        // NOLINTNEXTLINE(cppcoreguidelines-avoid-c-arrays, hicpp-avoid-c-arrays, modernize-avoid-c-arrays)
+        bool StartCef(int argc, char* argv[], uint32_t screenWidth, uint32_t screenHeight);
         void StopCef();
 
         // TODO: This probably shouldn't be architected this way.
@@ -37,28 +31,28 @@ namespace CE
         const CefRect& GetPopupRect();
         bool HasPopup();
 
-		// EventListener Interface
-		void OnEvent(const Event& event) override;
+        // EventListener Interface
+        void OnEvent(const Event& event) override;
 
-	private:
-	    void HandleSdlEvent(const Event& event);
+    private:
+        void HandleSdlEvent(const Event& event);
         unsigned GetSdlCefInputModifiers(const SDL_Event& event);
 
 #ifdef _WIN32
-		void HandleWindowsMessageEvent(const Event& event);
-		bool IsKeyDown(WPARAM wParam);
-		int GetNativeCefKeyboardModifiers(WPARAM wParam, LPARAM lParam);
-		void ToggleDevToolsWindow();
+        void HandleWindowsMessageEvent(const Event& event);
+        bool IsKeyDown(WPARAM wParam);
+        int GetNativeCefKeyboardModifiers(WPARAM wParam, LPARAM lParam);
+        void ToggleDevToolsWindow();
 #endif
 
-		EventSystem* eventSystem;
+        EventSystem* eventSystem;
         SDL_Window* window;
 
         CefRefPtr<UIClient> uiClient;
         CefRefPtr<CefBrowser> browser;
         UIExternalMessagePump* externalMessagePump;
         UIQueryHandler* queryHandler;
-	};
+    };
 }
 
 #endif //_CE_CEF_MAIN_H_
